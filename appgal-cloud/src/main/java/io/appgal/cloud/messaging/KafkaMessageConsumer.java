d53705cbe850e3fc8ec57817b5f4a068fd11eacd
babyboy <bugs.bunny.shah@gmail.com>
2020-02-05 10:02:26 -0600

    messaging > kafka > MessageConsumer > Getting Started

--- appgal-cloud/src/main/java/io/appgal/cloud/messaging/KafkaMessageConsumer.java
+++ appgal-cloud/src/main/java/io/appgal/cloud/messaging/KafkaMessageConsumer.java
@@ -1,13 +1,44 @@
 package io.appgal.cloud.messaging;
 
+import com.google.gson.JsonDeserializer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
 
 @ApplicationScoped
 public class KafkaMessageConsumer {
     private static Logger logger = LoggerFactory.getLogger(KafkaMessageConsumer.class);
+
+    private KafkaConsumer<String,String> kafkaConsumer;
+
+    public KafkaMessageConsumer()
+    {
+
+    }
+
+    @PostConstruct
+    public void start() throws UnknownHostException {
+        Properties config = new Properties();
+        config.put("client.id", InetAddress.getLocalHost().getHostName());
+        config.put("group.id", "foo");
+        config.put("bootstrap.servers", "host1:9092,host2:9092");
+        config.put("key.deserializer", org.apache.kafka.common.serialization.StringDeserializer.class);
+        config.put("value.deserializer", org.springframework.kafka.support.serializer.JsonDeserializer.class);
+        this.kafkaConsumer = new KafkaConsumer<String, String>(config);
+    }
+
+    @PreDestroy
+    public void stop()
+    {
+        this.kafkaConsumer.close();
+    }
 
     public void consumeData()
     {
