92cfd5c04005c5a8d2110ecfc66d9b2326cd8fb3
babyboy <bugs.bunny.shah@gmail.com>
2020-02-10 16:04:05 -0600

    FoodRunnerSyncProtocol > Implementation > getting_started

--- appgal-cloud/src/test/java/io/appgal/cloud/model/SourceNotificationsTest.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/model/SourceNotificationsTest.java
@@ -1,4 +1,14 @@
 package io.appgal.cloud.model;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SourceNotificationsTest {
+    private static Logger logger = LoggerFactory.getLogger(SourceNotificationsTest.class);
+
+    @Test
+    public void test()
+    {}
+
 }
