35e5ba3c27e60ce43fcaa48563e457388736a756
babyboy <bugs.bunny.shah@gmail.com>
2020-02-12 08:25:03 -0600

    FoodRunnerSyncProtocol > Implementation > end_to_end > endpoint_to_kafkalog > tests_adding_assertions

--- appgal-cloud/src/test/java/io/appgal/cloud/messaging/KafkaMessagingTests.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/messaging/KafkaMessagingTests.java
@@ -1,17 +1,24 @@
 package io.appgal.cloud.messaging;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.inject.Inject;
 import java.net.UnknownHostException;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 //TODO:Look at removing dependency on KafkaServer (@bugs.bunny.shah@gmail.com)
@@ -26,27 +33,40 @@ public class KafkaMessagingTests {
     @Test
     public void testReadNotifications() throws InterruptedException, UnknownHostException {
 
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("sourceNotificationId", UUID.randomUUID().toString()+"/sourceNotificationId");
-        this.kafkaDaemonClient.produceData(jsonObject);
-        Thread.sleep(10000);
-
-        //OffsetDateTime start = OffsetDateTime.now(ZoneOffset.UTC).minusHours(Duration.ofHours(6).toHours());
         OffsetDateTime start = OffsetDateTime.now(ZoneOffset.UTC);
         OffsetDateTime end = start.plusMinutes(Duration.ofMinutes(10).toMinutes());
         MessageWindow messageWindow = new MessageWindow(start, end);
         this.kafkaDaemonClient.readNotifications(messageWindow);
 
+        JsonObject jsonObject = new JsonObject();
+        List<String> ids = new ArrayList<>();
         for(int i=0; i< 10; i++) {
             jsonObject = new JsonObject();
-            jsonObject.addProperty("sourceNotificationId", UUID.randomUUID().toString()+"/sourceNotificationId");
+            String id = UUID.randomUUID().toString();
+            ids.add(id);
+            jsonObject.addProperty("sourceNotificationId", id);
             this.kafkaDaemonClient.produceData(jsonObject);
         }
 
         Thread.sleep(30000);
 
+        JsonArray messages = messageWindow.getMessages();
         logger.info("***************TIME_TO_ASSERT***************************");
-        logger.info(messageWindow.getMessages().toString());
+        logger.info(messages.toString());
         logger.info("********************************************************");
+
+        //assert the size
+        int idMatchCount = 0;
+        Iterator<JsonElement> iterator = messages.iterator();
+        while(iterator.hasNext())
+        {
+            JsonObject local = iterator.next().getAsJsonObject();
+            String localId = local.get("sourceNotificationId").getAsString();
+            if(ids.contains(localId))
+            {
+                idMatchCount++;
+            }
+        }
+        assertEquals(ids.size(), idMatchCount);
     }
 }
