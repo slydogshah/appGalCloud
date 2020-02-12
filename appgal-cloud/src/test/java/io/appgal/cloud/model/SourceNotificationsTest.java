26dc755442c0ba3697607a8b0b9ef718e11d38b2
babyboy <bugs.bunny.shah@gmail.com>
2020-02-12 08:34:03 -0600

    FoodRunnerSyncProtocol > Implementation > end_to_end > endpoint_to_kafkalog > tests_adding_assertions

--- appgal-cloud/src/test/java/io/appgal/cloud/model/SourceNotificationsTest.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/model/SourceNotificationsTest.java
@@ -1,9 +1,14 @@
 package io.appgal.cloud.model;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import io.appgal.cloud.messaging.MessageWindow;
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
 import java.time.OffsetDateTime;
@@ -18,13 +23,24 @@ public class SourceNotificationsTest {
     {
         OffsetDateTime start = OffsetDateTime.now(ZoneOffset.UTC);
         OffsetDateTime end = start.plusMinutes(Duration.ofMinutes(10).toMinutes());
+        long startTimestamp = start.toEpochSecond();
+        long endTimestamp = end.toEpochSecond();
         MessageWindow messageWindow = new MessageWindow(start, end);
 
+        String sourceNotificationId = UUID.randomUUID().toString();
         SourceNotification sourceNotification = new SourceNotification();
-        sourceNotification.setSourceNotificationId(UUID.randomUUID().toString());
+        sourceNotification.setSourceNotificationId(sourceNotificationId);
         sourceNotification.setMessageWindow(messageWindow);
 
+        JsonObject json = JsonParser.parseString(sourceNotification.toString()).getAsJsonObject();
+        logger.info("****");
         logger.info(sourceNotification.toString());
+        logger.info("****");
+
+        //assert the state
+        assertEquals(sourceNotificationId, json.get("sourceNotificationId").getAsString());
+        assertEquals(startTimestamp, json.get("startTimestamp").getAsLong());
+        assertEquals(endTimestamp, json.get("endTimestamp").getAsLong());
     }
 
 }
