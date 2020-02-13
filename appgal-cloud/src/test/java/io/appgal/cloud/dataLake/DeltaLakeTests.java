298031527c7ed1def7b2654173748eb74c03dfa1
babyboy <bugs.bunny.shah@gmail.com>
2020-02-13 12:49:26 -0600

    DeltaLake > Spark > getting_started_on_version_2.0

---
+++ appgal-cloud/src/test/java/io/appgal/cloud/dataLake/DeltaLakeTests.java
@@ -0,0 +1,19 @@
+package io.appgal.cloud.dataLake;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public class DeltaLakeTests {
+    private static Logger logger = LoggerFactory.getLogger(DeltaLakeTests.class);
+
+    @Test
+    public void testHelloSpark()
+    {
+        SparkSession spark = SparkSession.builder().getOrCreate();
+    }
+}
