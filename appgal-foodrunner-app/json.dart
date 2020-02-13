e7fab29fce47c02ce7dc9568a6c26663b151c2d3
babyboy <bugs.bunny.shah@gmail.com>
2020-02-12 18:32:56 -0600

    json > json_framework > more

--- appgal-foodrunner-app/json.dart
+++ appgal-foodrunner-app/json.dart
@@ -8,4 +8,27 @@ void main(List<String> args)
 
   print('Howdy, ${user['name']}!');
   print('We sent the verification link to ${user['email']}.');
+
+  User model = new User("model", "model@model.com");
+  Map<String, dynamic> modelJson = model.toJson();
+
+  print('Howdy, ${modelJson['name']}!');
+  print('We sent the verification link to ${modelJson['email']}.');
+}
+
+class User {
+  final String name;
+  final String email;
+
+  User(this.name, this.email);
+
+  User.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        email = json['email'];
+
+  Map<String, dynamic> toJson() =>
+    {
+      'name': name,
+      'email': email,
+    };
 }
