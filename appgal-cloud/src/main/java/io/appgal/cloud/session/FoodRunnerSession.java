3f4ac3d92fb910f18a7318b4c01a905cb901020d
babyboy <bugs.bunny.shah@gmail.com>
2020-02-20 04:24:08 -0600

    AppGalCloud > destination_notification_lifecycle > end_to_end > food_runner_session_framework

---
+++ appgal-cloud/src/main/java/io/appgal/cloud/session/FoodRunnerSession.java
@@ -0,0 +1,10 @@
+package io.appgal.cloud.session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FoodRunnerSession {
+    private static Logger logger = LoggerFactory.getLogger(FoodRunnerSession.class);
+
+
+}
