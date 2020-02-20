fc94283134b6e3eb95650d325e493befdeeef86f
babyboy <bugs.bunny.shah@gmail.com>
2020-02-20 06:14:18 -0600

    AppGalCloud > destination_notification_lifecycle > end_to_end > food_runner_session_framework

--- appgal-cloud/src/test/java/io/appgal/cloud/session/FoodRunnerSessionTests.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/session/FoodRunnerSessionTests.java
@@ -1,5 +1,10 @@
 package io.appgal.cloud.session;
 
+
+import io.appgal.cloud.foodRunnerSync.protocol.ProcessIncomingPackets;
+import io.appgal.cloud.messaging.KafkaDaemon;
+import io.appgal.cloud.messaging.MessageWindow;
+
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.time.*;
 
 @QuarkusTest
 public class FoodRunnerSessionTests {
@@ -16,10 +22,15 @@ public class FoodRunnerSessionTests {
     @Inject
     private FoodRunnerSession foodRunnerSession;
 
+    @Inject
+    private ProcessIncomingPackets processIncomingPackets;
+
+    @Inject
+    private KafkaDaemon kafkaDaemon;
+
     @BeforeEach
     private void setUp()
     {
-        this.foodRunnerSession.start();
     }
 
     @AfterEach
@@ -28,7 +39,28 @@ public class FoodRunnerSessionTests {
     }
 
     @Test
-    public void testStart()
+    public void testStart() throws Exception
     {
+        this.kafkaDaemon.logStartUp();
+        int counter=0;
+        while(!this.kafkaDaemon.getActive()) {
+            Thread.sleep(5000);
+            if(counter++ == 3)
+            {
+                break;
+            }
+        }
+
+        //OffsetDateTime start = OffsetDateTime.now(ZoneOffset.UTC);
+        //OffsetDateTime end = start.plusMinutes(Duration.ofMinutes(10).toMinutes());
+        //MessageWindow messageWindow = new MessageWindow(start, end);
+        LocalDateTime startOfLocalDateInUtc = LocalDate.now(ZoneOffset.UTC).atStartOfDay();
+        OffsetDateTime startTime = OffsetDateTime.of(startOfLocalDateInUtc, ZoneOffset.UTC);
+        OffsetDateTime endTime = OffsetDateTime.now(ZoneOffset.UTC);
+        MessageWindow messageWindow = new MessageWindow(startTime, endTime);
+
+        this.processIncomingPackets.processSourceNotification(messageWindow);
+
+        this.foodRunnerSession.start();
     }
 }
