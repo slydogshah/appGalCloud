872ad932afc60d1612c2fc94bc7c64315141bb3f
babyboy <bugs.bunny.shah@gmail.com>
2020-02-24 15:21:40 -0600

    AppGalCloud > destination_notification_lifecycle > end_to_end > food_runner_session_geospatial_research

--- appgal-cloud/src/test/java/io/appgal/cloud/geospatial/TestDriveTests.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/geospatial/TestDriveTests.java
@@ -1,55 +1,47 @@
 package io.appgal.cloud.geospatial;
 
-import org.geotools.data.DataUtilities;
+import org.geotools.data.*;
+import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.shapefile.ShapefileDataStore;
 import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.action.SafeAction;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class TestDriveTests {
     private static Logger logger = LoggerFactory.getLogger(TestDriveTests.class);
 
     @Test
-    public void testSimpleFeatureType() throws Exception
-    {
-        // Set cross-platform look & feel for compatability
-        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-
-        String location = Thread.currentThread().getContextClassLoader().getResource("location.csv").getFile();
-        File file = new File(location);
-
-        final SimpleFeatureType TYPE =
-                DataUtilities.createType(
-                        "Location",
-                        "the_geom:Point:srid=4326,"
-                                + // <- the geometry attribute: Point type
-                                "name:String,"
-                                + // <- a String attribute
-                                "number:Integer" // a number attribute
-                );
-        logger.info("TYPE:" + TYPE);
-    }
-
-    @Test
-    public void testSimpleFeatureCreation() throws Exception
+    public void testSimpleFeaturePersistence() throws Exception
     {
         // Set cross-platform look & feel for compatability
         UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
@@ -105,20 +97,6 @@ public class TestDriveTests {
                 }
             }
         }
-    }
-
-    @Test
-    public void testSimpleFeaturePersistence() throws Exception
-    {
-        final SimpleFeatureType TYPE =
-                DataUtilities.createType(
-                        "Location",
-                        "the_geom:Point:srid=4326,"
-                                + // <- the geometry attribute: Point type
-                                "name:String,"
-                                + // <- a String attribute
-                                "number:Integer" // a number attribute
-                );
 
         /*
          * Get an output file name and create the new shapefile
@@ -138,5 +116,116 @@ public class TestDriveTests {
          * TYPE is used as a template to describe the file contents
          */
         newDataStore.createSchema(TYPE);
+
+        /*
+         * Write the features to the shapefile
+         */
+        Transaction transaction = new DefaultTransaction("create");
+
+        String typeName = newDataStore.getTypeNames()[0];
+        SimpleFeatureSource featureSource = newDataStore.getFeatureSource(typeName);
+        SimpleFeatureType SHAPE_TYPE = featureSource.getSchema();
+        /*
+         * The Shapefile format has a couple limitations:
+         * - "the_geom" is always first, and used for the geometry attribute name
+         * - "the_geom" must be of type Point, MultiPoint, MuiltiLineString, MultiPolygon
+         * - Attribute names are limited in length
+         * - Not all data types are supported (example Timestamp represented as Date)
+         *
+         * Each data store has different limitations so check the resulting SimpleFeatureType.
+         */
+        logger.info("SHAPE:" + SHAPE_TYPE);
+
+        SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
+        /*
+         * SimpleFeatureStore has a method to add features from a
+         * SimpleFeatureCollection object, so we use the ListFeatureCollection
+         * class to wrap our list of features.
+         */
+        SimpleFeatureCollection collection = new ListFeatureCollection(TYPE, features);
+        featureStore.setTransaction(transaction);
+        try {
+            featureStore.addFeatures(collection);
+            transaction.commit();
+        } catch (Exception problem) {
+            problem.printStackTrace();
+            transaction.rollback();
+        } finally {
+            transaction.close();
+        }
+    }
+
+    @Test
+    public void testUnderstandShapes() throws Exception
+    {
+        File sourceFile = new File("simpleFeaturePersistence.shp");
+        FileDataStore store = FileDataStoreFinder.getDataStore(sourceFile);
+        SimpleFeatureSource featureSource = store.getFeatureSource();
+        String typeName = store.getTypeNames()[0];
+
+        //Get the Geometry List
+        SimpleFeatureCollection features = featureSource.getFeatures();
+        SimpleFeatureType schema = featureSource.getSchema();
+
+        // Create a map context and add our shapefile to it
+        MapContent map = new MapContent();
+        final Style type = SLD.createSimpleStyle(store, typeName, Color.BLACK);
+        Layer layer = new FeatureLayer(featureSource, type);
+        map.layers().add(layer);
+
+        // Create a JMapFrame with custom toolbar buttons
+        JMapFrame mapFrame = new JMapFrame(map);
+        mapFrame.enableToolBar(true);
+        mapFrame.enableStatusBar(true);
+
+        JToolBar toolbar = mapFrame.getToolBar();
+        toolbar.addSeparator();
+        toolbar.add(new JButton(new ValidateGeometryAction(features)));
+        //toolbar.add(new JButton(new ExportShapefileAction()));
+
+        // Display the map frame. When it is closed the application will exit
+        mapFrame.setSize(800, 600);
+        mapFrame.setVisible(true);
+
+        while(true);
+    }
+
+    class ValidateGeometryAction extends SafeAction {
+        private SimpleFeatureCollection simpleFeatureCollection;
+
+        ValidateGeometryAction(SimpleFeatureCollection simpleFeatureCollection) {
+            super("Validate geometry");
+            this.simpleFeatureCollection = simpleFeatureCollection;
+            putValue(Action.SHORT_DESCRIPTION, "Check each geometry");
+        }
+
+        public void action(ActionEvent e) throws Throwable {
+            int numInvalid = validateFeatureGeometry(simpleFeatureCollection);
+            String msg;
+            if (numInvalid == 0) {
+                msg = "All feature geometries are valid";
+            } else {
+                msg = "Invalid geometries: " + numInvalid;
+            }
+            JOptionPane.showMessageDialog(
+                    null, msg, "Geometry results", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        private int validateFeatureGeometry(SimpleFeatureCollection simpleFeatureCollection) {
+
+            int numberValid = 0;
+
+            SimpleFeatureIterator iterator = simpleFeatureCollection.features();
+            while(iterator.hasNext())
+            {
+                SimpleFeature feature = iterator.next();
+
+                logger.info(feature.toString());
+
+                numberValid ++;
+            }
+
+            return numberValid;
+        }
     }
 }
