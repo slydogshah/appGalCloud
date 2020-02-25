7651c726a8bb1679ffe613aa3cb65e53b9923054
babyboy <bugs.bunny.shah@gmail.com>
2020-02-25 14:03:20 -0600

    json_parsing_tests_source_notications

--- appgal-foodrunner-app/messaging/notificationReceiver.dart
+++ appgal-foodrunner-app/messaging/notificationReceiver.dart
@@ -3,12 +3,12 @@ import 'dart:convert';
 
 class NotificationReceiver
 {
-  Map<String, String> getSourceNotifications()
+  String json = "[{\"sourceNotificationId\":\"92ed655a-99a2-438b-8eeb-05d12a2d8a1b\",\"startTimestamp\":1582660041,\"endTimestamp\":1582660641,\"latitude\":\"44.9441\",\"longitude\":\"-93.0852\"}]";
+
+
+  List<dynamic> getSourceNotifications()
   {
-      Map<String, String> sourceNotifications = new HashMap<String, String>(); 
-
-      sourceNotifications.putIfAbsent("sourceNotificationId", () => "blah");
-
+      List<dynamic> sourceNotifications = jsonDecode(this.json); 
       return sourceNotifications;
   }
 }
