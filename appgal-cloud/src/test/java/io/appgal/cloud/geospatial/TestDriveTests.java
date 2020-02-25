3570e4644a72a749ff89a531c92255a258edd968
babyboy <bugs.bunny.shah@gmail.com>
2020-02-25 09:30:38 -0600

    AppGalCloud > destination_notification_lifecycle > end_to_end > food_runner_session_geospatial_research

--- appgal-cloud/src/test/java/io/appgal/cloud/geospatial/TestDriveTests.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/geospatial/TestDriveTests.java
@@ -9,10 +9,16 @@ import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.data.simple.SimpleFeatureStore;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.geometry.jts.JTS;
 import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.Layer;
 import org.geotools.map.MapContent;
+import org.geotools.referencing.GeodeticCalculator;
+import org.geotools.referencing.crs.DefaultEngineeringCRS;
+import org.geotools.referencing.crs.DefaultGeocentricCRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.geotools.referencing.cs.DefaultEllipsoidalCS;
 import org.geotools.styling.SLD;
 import org.geotools.styling.Style;
 import org.geotools.swing.JMapFrame;
@@ -24,6 +30,7 @@ import org.locationtech.jts.geom.Point;
 import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,191 +48,47 @@ public class TestDriveTests {
     private static Logger logger = LoggerFactory.getLogger(TestDriveTests.class);
 
     @Test
-    public void testSimpleFeaturePersistence() throws Exception
+    public void testCalculateDistance() throws Exception
     {
-        // Set cross-platform look & feel for compatability
-        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        double startLatitude = 30.25860595703125d;
+        double startLongitude = -97.74873352050781d;
 
-        String location = Thread.currentThread().getContextClassLoader().getResource("location.csv").getFile();
-        File file = new File(location);
+        //double endLatitude = 46.066667d;
+        //double endLongitude = 11.116667d;
 
-        final SimpleFeatureType TYPE =
-                DataUtilities.createType(
-                        "Location",
-                        "the_geom:Point:srid=4326,"
-                                + // <- the geometry attribute: Point type
-                                "name:String,"
-                                + // <- a String attribute
-                                "number:Integer" // a number attribute
-                );
+        double endLatitude = 30.234689712524414d;
+        double endLongitude = -97.71511840820312d;
 
-        /*
-         * A list to collect features as we create them.
-         */
-        List<SimpleFeature> features = new ArrayList<>();
+        Coordinate start = new Coordinate();
+        start.setX(startLatitude);
+        start.setY(startLongitude);
 
-        /*
-         * GeometryFactory will be used to create the geometry attribute of each feature,
-         * using a Point object for the location.
-         */
-        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+        Coordinate end = new Coordinate();
+        end.setX(endLatitude);
+        end.setY(endLongitude);
 
-        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(TYPE);
+        CoordinateReferenceSystem crs = DefaultGeographicCRS.WGS84;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            /* First line of the data file is the header */
-            String line = reader.readLine();
-            System.out.println("Header: " + line);
+        //CoordinateReferenceSystem crs =  DefaultGeocentricCRS.CARTESIAN;
 
-            for (line = reader.readLine(); line != null; line = reader.readLine()) {
-                if (line.trim().length() > 0) { // skip blank lines
-                    String tokens[] = line.split("\\,");
+        //CoordinateReferenceSystem crs = DefaultEngineeringCRS.CARTESIAN_2D;
 
-                    double latitude = Double.parseDouble(tokens[0]);
-                    double longitude = Double.parseDouble(tokens[1]);
-                    String name = tokens[2].trim();
-                    int number = Integer.parseInt(tokens[3].trim());
+        //CoordinateReferenceSystem crs = DefaultEngineeringCRS.GENERIC_2D;
 
-                    /* Longitude (= x coord) first ! */
-                    Point point = geometryFactory.createPoint(new Coordinate(longitude, latitude));
+        // the following code is based on JTS.orthodromicDistance( start, end, crs )
+        GeodeticCalculator gc = new GeodeticCalculator(crs);
+        gc.setStartingPosition(JTS.toDirectPosition(start, crs));
+        gc.setDestinationPosition(JTS.toDirectPosition(end, crs));
 
-                    featureBuilder.add(point);
-                    featureBuilder.add(name);
-                    featureBuilder.add(number);
-                    SimpleFeature feature = featureBuilder.buildFeature(null);
-                    features.add(feature);
-                }
-            }
-        }
+        double distance = gc.getOrthodromicDistance();
 
-        /*
-         * Get an output file name and create the new shapefile
-         */
-        File newFile = new File("simpleFeaturePersistence.shp");
+        int totalmeters = (int) distance;
+        int km = totalmeters / 1000;
+        int meters = totalmeters - (km * 1000);
+        float remaining_cm = (float) (distance - totalmeters) * 10000;
+        remaining_cm = Math.round(remaining_cm);
+        float cm = remaining_cm / 100;
 
-        ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
-
-        Map<String, Serializable> params = new HashMap<>();
-        params.put("url", newFile.toURI().toURL());
-        params.put("create spatial index", Boolean.TRUE);
-
-        ShapefileDataStore newDataStore =
-                (ShapefileDataStore) dataStoreFactory.createNewDataStore(params);
-
-        /*
-         * TYPE is used as a template to describe the file contents
-         */
-        newDataStore.createSchema(TYPE);
-
-        /*
-         * Write the features to the shapefile
-         */
-        Transaction transaction = new DefaultTransaction("create");
-
-        String typeName = newDataStore.getTypeNames()[0];
-        SimpleFeatureSource featureSource = newDataStore.getFeatureSource(typeName);
-        SimpleFeatureType SHAPE_TYPE = featureSource.getSchema();
-        /*
-         * The Shapefile format has a couple limitations:
-         * - "the_geom" is always first, and used for the geometry attribute name
-         * - "the_geom" must be of type Point, MultiPoint, MuiltiLineString, MultiPolygon
-         * - Attribute names are limited in length
-         * - Not all data types are supported (example Timestamp represented as Date)
-         *
-         * Each data store has different limitations so check the resulting SimpleFeatureType.
-         */
-        logger.info("SHAPE:" + SHAPE_TYPE);
-
-        SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
-        /*
-         * SimpleFeatureStore has a method to add features from a
-         * SimpleFeatureCollection object, so we use the ListFeatureCollection
-         * class to wrap our list of features.
-         */
-        SimpleFeatureCollection collection = new ListFeatureCollection(TYPE, features);
-        featureStore.setTransaction(transaction);
-        try {
-            featureStore.addFeatures(collection);
-            transaction.commit();
-        } catch (Exception problem) {
-            problem.printStackTrace();
-            transaction.rollback();
-        } finally {
-            transaction.close();
-        }
-    }
-
-    @Test
-    public void testUnderstandShapes() throws Exception
-    {
-        File sourceFile = new File("simpleFeaturePersistence.shp");
-        FileDataStore store = FileDataStoreFinder.getDataStore(sourceFile);
-        SimpleFeatureSource featureSource = store.getFeatureSource();
-        String typeName = store.getTypeNames()[0];
-
-        //Get the Geometry List
-        SimpleFeatureCollection features = featureSource.getFeatures();
-        SimpleFeatureType schema = featureSource.getSchema();
-
-        // Create a map context and add our shapefile to it
-        MapContent map = new MapContent();
-        final Style type = SLD.createSimpleStyle(store, typeName, Color.BLACK);
-        Layer layer = new FeatureLayer(featureSource, type);
-        map.layers().add(layer);
-
-        // Create a JMapFrame with custom toolbar buttons
-        JMapFrame mapFrame = new JMapFrame(map);
-        mapFrame.enableToolBar(true);
-        mapFrame.enableStatusBar(true);
-
-        JToolBar toolbar = mapFrame.getToolBar();
-        toolbar.addSeparator();
-        toolbar.add(new JButton(new ValidateGeometryAction(features)));
-        //toolbar.add(new JButton(new ExportShapefileAction()));
-
-        // Display the map frame. When it is closed the application will exit
-        mapFrame.setSize(800, 600);
-        mapFrame.setVisible(true);
-
-        while(true);
-    }
-
-    class ValidateGeometryAction extends SafeAction {
-        private SimpleFeatureCollection simpleFeatureCollection;
-
-        ValidateGeometryAction(SimpleFeatureCollection simpleFeatureCollection) {
-            super("Validate geometry");
-            this.simpleFeatureCollection = simpleFeatureCollection;
-            putValue(Action.SHORT_DESCRIPTION, "Check each geometry");
-        }
-
-        public void action(ActionEvent e) throws Throwable {
-            int numInvalid = validateFeatureGeometry(simpleFeatureCollection);
-            String msg;
-            if (numInvalid == 0) {
-                msg = "All feature geometries are valid";
-            } else {
-                msg = "Invalid geometries: " + numInvalid;
-            }
-            JOptionPane.showMessageDialog(
-                    null, msg, "Geometry results", JOptionPane.INFORMATION_MESSAGE);
-        }
-
-        private int validateFeatureGeometry(SimpleFeatureCollection simpleFeatureCollection) {
-
-            int numberValid = 0;
-
-            SimpleFeatureIterator iterator = simpleFeatureCollection.features();
-            while(iterator.hasNext())
-            {
-                SimpleFeature feature = iterator.next();
-
-                logger.info(feature.toString());
-
-                numberValid ++;
-            }
-
-            return numberValid;
-        }
+        logger.info("Distance = " + km + "km " + meters + "m " + cm + "cm");
     }
 }
