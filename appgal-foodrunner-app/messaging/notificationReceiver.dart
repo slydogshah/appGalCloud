086ce6b999006092ef358bc8294386e56e91faaa
babyboy <bugs.bunny.shah@gmail.com>
2020-02-25 12:14:32 -0600

    feature: foodrunner_notification_to_fresh_food_source_they_are_on_their_way

---
+++ appgal-foodrunner-app/messaging/notificationReceiver.dart
@@ -0,0 +1,17 @@
+import 'package:test/test.dart';
+
+void main(List<String> args) {
+  test('String.split() splits the string on the delimiter', () {
+    var string = 'foo,bar,baz';
+    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  });
+
+  test('String.trim() removes surrounding whitespace', () {
+    var string = '  foo ';
+    expect(string.trim(), equals('foo'));
+  });
+}
+
+class NotificationReceiver
+{
+}
