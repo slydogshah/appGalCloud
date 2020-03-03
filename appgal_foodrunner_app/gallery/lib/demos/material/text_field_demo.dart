553232fe221fba34a4daf11dd5cc4f9e3946a7cf
babyboy <bugs.bunny.shah@gmail.com>
2020-03-03 15:27:39 -0600

    minor_refactor

--- appgal_foodrunner_app/gallery/lib/demos/material/text_field_demo.dart
+++ appgal_foodrunner_app/gallery/lib/demos/material/text_field_demo.dart
@@ -9,6 +9,8 @@ import 'package:flutter/gestures.dart' show DragStartBehavior;
 
 import 'package:gallery/l10n/gallery_localizations.dart';
 
+import 'landingScene.dart';
+
 // BEGIN textFieldDemo
 
 class TextFieldDemo extends StatelessWidget {
@@ -293,24 +295,4 @@ class _UsNumberTextInputFormatter extends TextInputFormatter {
     );
   }
 }
-
-class FirstRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            // Navigate to second route when tapped.
-          },
-        ),
-      ),
-    );
-  }
-}
-
 // END
