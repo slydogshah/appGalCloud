d7a90c845fd33648cda6ae951d480d704404b3aa
babyboy <bugs.bunny.shah@gmail.com>
2020-03-07 14:23:51 -0600

    google_maps_integration_with_appgalCloud_in_progress

--- appgal_foodrunner_app/google_maps_integration/lib/main.dart
+++ appgal_foodrunner_app/google_maps_integration/lib/main.dart
@@ -1,37 +1,80 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:flutter_localized_locales/flutter_localized_locales.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_maps_integration/constants.dart';
+import 'package:google_maps_integration/data/gallery_options.dart';
+import 'package:google_maps_integration/l10n/gallery_localizations.dart';
+import 'package:google_maps_integration/pages/backdrop.dart';
+import 'package:google_maps_integration/pages/splash.dart';
+import 'package:google_maps_integration/themes/gallery_theme_data.dart';
 
-void main() => runApp(MyApp());
+void setOverrideForDesktop() {
+  if (kIsWeb) return;
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+  if (Platform.isMacOS) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  } else if (Platform.isFuchsia) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  GoogleMapController mapController;
+void main() {
+  setOverrideForDesktop();
+  GoogleFonts.config.allowHttp = false;
+  runApp(GalleryApp());
+}
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
+class GalleryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+    return ModelBinding(
+      initialModel: GalleryOptions(
+        themeMode: ThemeMode.system,
+        textScaleFactor: systemTextScaleFactorOption,
+        customTextDirection: CustomTextDirection.localeBased,
+        locale: null,
+        timeDilation: timeDilation,
+        platform: defaultTargetPlatform,
+      ),
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            title: 'Gallery',
+            debugShowCheckedModeBanner: false,
+            themeMode: GalleryOptions.of(context).themeMode,
+            theme: GalleryThemeData.lightThemeData.copyWith(
+              platform: GalleryOptions.of(context).platform,
+            ),
+            darkTheme: GalleryThemeData.darkThemeData.copyWith(
+              platform: GalleryOptions.of(context).platform,
+            ),
+            localizationsDelegates: [
+              ...GalleryLocalizations.localizationsDelegates,
+              LocaleNamesLocalizationsDelegate()
+            ],
+            supportedLocales: GalleryLocalizations.supportedLocales,
+            locale: GalleryOptions.of(context).locale,
+            localeResolutionCallback: (locale, supportedLocales) {
+              deviceLocale = locale;
+              return locale;
+            },
+            home: ApplyTextOptions(
+              child: SplashPage(
+                child: AnimatedBackdrop(),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
