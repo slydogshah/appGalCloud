d682bd54e01893571de4e2143647cc172910298f
babyboy <bugs.bunny.shah@gmail.com>
2020-03-17 01:36:55 -0500

    AppGalCloud > firebase_server_intergration_in_progress

--- appgal-cloud/src/main/java/io/appgal/cloud/rest/client/GoogleFireBaseClient.java
+++ appgal-cloud/src/main/java/io/appgal/cloud/rest/client/GoogleFireBaseClient.java
@@ -1,13 +1,16 @@
 package io.appgal.cloud.rest.client;
 
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 @ApplicationScoped
 public class GoogleFireBaseClient {
@@ -16,20 +19,38 @@ public class GoogleFireBaseClient {
     public void registerToken(String token)
     {
         try {
-            //String registrationUrl = "https://fcm.googleapis.com/v1/appgalMessagingExample/projects.messages";
-            String registrationUrl = "https://appgalMessagingExample.firebaseio.com/users/";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("registration_id",
+                    "AIzaSyAF5tpmmfkXGleTewkzy1ji7MTlCciOnfY");
+            jsonObject.addProperty("to", "cL9pKU-1ikU:APA91bHz3-Gqjkuqu-vXCk7Za7_IRcRxAZl6zwes5-gqS1xnHG4dqggnJRUa0wJOx3x_rC2jKBWh7ka9XJvVKN38yN8t1T5gvYSdYwvUBKcRm3Hsfa8uJo6ntjdDpdDGCDloYkjraCsV");
+            String body = jsonObject.toString();
+            logger.info("....");
+            logger.info(body);
+            logger.info("....");
+
+            String registrationUrl = "https://fcm.googleapis.com/fcm/send";
+            //String registrationUrl = "https://fcm.googleapis.com/v1/projects/appgalMessagingExample/messages:send";
             URI requestUri = new URI(registrationUrl);
 
             RestTemplate restTemplate = new RestTemplate();
-            //RequestEntity<String> requestEntity = RequestEntity.post(requestUri).body("{}");
-            RequestEntity<Void> requestEntity = RequestEntity.get(requestUri).build();
+            RequestEntity<String> requestEntity = RequestEntity.post(requestUri)
+                    .header("Authorization",
+                            "key=AIzaSyAF5tpmmfkXGleTewkzy1ji7MTlCciOnfY")
+                    .header("Content-Type","application/json")
+                    .body(body);
+            //RequestEntity<Void> requestEntity = RequestEntity.get(requestUri).build();
             ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
 
-            logger.info("****");
-            logger.info(responseEntity.getBody());
+            logger.info("**RESPONSE**");
+            logger.info(responseEntity.toString());
             logger.info("****");
         }
-        catch(Exception e)
+        catch(HttpClientErrorException e)
+        {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+        catch(URISyntaxException e)
         {
             logger.error(e.getMessage(), e);
             throw new RuntimeException(e);
