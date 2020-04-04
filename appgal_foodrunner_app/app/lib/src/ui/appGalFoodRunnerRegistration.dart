3fcbe97087272caac6d0799ce88673a893ab4943
babyboy <bugs.bunny.shah@gmail.com>
2020-04-04 11:30:02 -0500

    registration_scene_in_progress

--- appgal_foodrunner_app/app/lib/src/ui/appGalFoodRunnerRegistration.dart
+++ appgal_foodrunner_app/app/lib/src/ui/appGalFoodRunnerRegistration.dart
@@ -8,27 +8,12 @@ class CommonThings {
   static Size size;
 }
 
-class MyApp extends StatelessWidget {
+class RegistrationScene extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'MediaQuery Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: new MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    CommonThings.size = MediaQuery.of(context).size;
-    print('Width of the screen: ${CommonThings.size.width}');
-    return new Container(
-      child: new Text("blah")
-    );
+    Text text = new Text("Hello World", textDirection: TextDirection.ltr,);
+    Center center = new Center(child: text,);
+    return center;
   }
 }
