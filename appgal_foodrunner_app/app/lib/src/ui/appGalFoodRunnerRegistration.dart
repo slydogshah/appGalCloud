f22b82f576c56006cfa327f6363e529a5ab54f05
babyboy <bugs.bunny.shah@gmail.com>
2020-04-04 12:32:32 -0500

    registration_scene_in_progress

--- appgal_foodrunner_app/app/lib/src/ui/appGalFoodRunnerRegistration.dart
+++ appgal_foodrunner_app/app/lib/src/ui/appGalFoodRunnerRegistration.dart
@@ -2,16 +2,72 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class RegistrationScene extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Text text = new Text("Hello World", textDirection: TextDirection.ltr,);
+    PasswordField passwordField = new PasswordField(fieldKey: new Key("0"), hintText: "Password", labelText: "Password",
+    helperText: "Password", obscureText: "Password",);
     AppBar appBar = new AppBar(automaticallyImplyLeading: false, title: new Text("Register"),);
-    Scaffold scaffold = new Scaffold(appBar: appBar, body: text,);
+    Scaffold scaffold = new Scaffold(appBar: appBar, body: passwordField,);
     MaterialApp materialApp = new MaterialApp(home: scaffold);
     return materialApp;
+  }
+}
+
+class PasswordField extends StatelessWidget {
+  const PasswordField({
+    this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.obscureText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+  });
+
+  final Key fieldKey;
+  final String hintText;
+  final String labelText;
+  final String helperText;
+  final String obscureText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: this.fieldKey,
+      cursorColor: Theme.of(context).cursorColor,
+      maxLength: 8,
+      onSaved: this.onSaved,
+      validator: this.validator,
+      onFieldSubmitted: this.onFieldSubmitted,
+      decoration: InputDecoration(
+        filled: true,
+        hintText: this.hintText,
+        labelText: this.labelText,
+        helperText: this.helperText,
+        suffixIcon: GestureDetector(
+          dragStartBehavior: DragStartBehavior.down,
+          //onTap: () {
+          //  setState(() {
+          //    _obscureText = !_obscureText;
+          //  });
+          //},
+          child: Icon(
+            true ? Icons.visibility : Icons.visibility_off,  //(lol)
+            semanticLabel: true
+                ? "Visible"
+                : "Hidden",
+          ),
+        ),
+      ),
+    );
   }
 }
