ae0753aff569717c70c4fae9fc31ec7147d5186f
babyboy <bugs.bunny.shah@gmail.com>
2020-04-03 22:57:21 -0500

    refactoring_and_code_cleanup

--- appgal_foodrunner_app/google_maps_integration/android/app/src/main/java/io/appgal/notifications/futureGeniuses/NotificationSettings.java
+++
@@ -1,6 +0,0 @@
-package io.appgal.notifications.futureGeniuses;
-
-public class NotificationSettings {
-    public static String HubName = "appgallabscluster";
-    public static String HubListenConnectionString = "Endpoint=sb://appgallabscluster.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=nZHaF6TrGdJhocifbwexNtTpimJ/+YDUtBYWyNBrIto=";
-}
