d6c01a13335453f016f828ada4a52eb543c697ef
babyboy <bugs.bunny.shah@gmail.com>
2020-04-09 12:16:49 -0500

    google_maps_integration_android

--- appgal_foodrunner_app/app/lib/src/ui/landingScene.dart
+++ appgal_foodrunner_app/app/lib/src/ui/landingScene.dart
@@ -1,21 +1,36 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class LandingScene extends StatelessWidget {
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(
-         title: Text('First Route'),
-       ),
-       body: Center(
-         child: RaisedButton(
-           child: Text('Open route'),
-           onPressed: () {
-             // Navigate to second route when tapped.
-           },
-         ),
-       ),
-     );
-   }
- }
+class LandingScene extends StatefulWidget {
+  @override
+  LandingSceneState createState() => LandingSceneState();
+}
+
+class LandingSceneState extends State<LandingScene> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
