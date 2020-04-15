525a19bb93fba8f59db88f50810b1df0739ec18a
babyboy <bugs.bunny.shah@gmail.com>
2020-04-15 08:28:21 -0500

    drop_down

--- appgal_foodrunner_app/app/lib/src/ui/login.dart
+++ appgal_foodrunner_app/app/lib/src/ui/login.dart
@@ -164,6 +164,13 @@ class RegistrationScene extends StatelessWidget {
     final cursorColor = Theme.of(context).cursorColor;
     const sizedBoxSpace = SizedBox(height: 24);
     ProfileFunctions profileFunctions = new ProfileFunctions();
+    List<Item> users = <Item>[
+    const Item('Android',Icon(Icons.android,color:  const Color(0xFF167F67),)),
+    const Item('Flutter',Icon(Icons.flag,color:  const Color(0xFF167F67),)),
+    const Item('ReactNative',Icon(Icons.format_indent_decrease,color:  const Color(0xFF167F67),)),
+    const Item('iOS',Icon(Icons.mobile_screen_share,color:  const Color(0xFF167F67),)),
+    ];
+    Item selectedUser;
     Scrollbar scrollbar = new Scrollbar(child: SingleChildScrollView(
             dragStartBehavior: DragStartBehavior.down,
             padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -198,6 +205,33 @@ class RegistrationScene extends StatelessWidget {
                     labelText:
                         "Mobile",
                   )
+                ),
+                sizedBoxSpace,
+                Center(
+                  child:  DropdownButton<Item>(
+                    hint:  Text("Select item"),
+                    value: selectedUser,
+                    onChanged: (Item Value) {
+                      /*setState(() {
+                        selectedUser = Value;
+                      });*/
+                    },
+                    items: users.map((Item user) {
+                      return  DropdownMenuItem<Item>(
+                        value: user,
+                        child: Row(
+                          children: <Widget>[
+                            user.icon,
+                            SizedBox(width: 10,),
+                            Text(
+                              user.name,
+                              style:  TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
                 ),
                 sizedBoxSpace,
                 Center(
@@ -258,4 +292,62 @@ class ProfileFunctions
       MaterialPageRoute(builder: (context) => new LandingScene(response)));
     });
   }  
+}
+
+class DropdownScreen extends StatefulWidget {
+  State createState() =>  DropdownScreenState();
+}
+class DropdownScreenState extends State<DropdownScreen> {
+  Item selectedUser;
+  List<Item> users = <Item>[
+    const Item('Android',Icon(Icons.android,color:  const Color(0xFF167F67),)),
+    const Item('Flutter',Icon(Icons.flag,color:  const Color(0xFF167F67),)),
+    const Item('ReactNative',Icon(Icons.format_indent_decrease,color:  const Color(0xFF167F67),)),
+    const Item('iOS',Icon(Icons.mobile_screen_share,color:  const Color(0xFF167F67),)),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home:  Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF167F67),
+          title: Text(
+            'Dropdown options',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body:  Center(
+          child:  DropdownButton<Item>(
+            hint:  Text("Select item"),
+            value: selectedUser,
+            onChanged: (Item Value) {
+              setState(() {
+                selectedUser = Value;
+              });
+            },
+            items: users.map((Item user) {
+              return  DropdownMenuItem<Item>(
+                value: user,
+                child: Row(
+                  children: <Widget>[
+                    user.icon,
+                    SizedBox(width: 10,),
+                    Text(
+                      user.name,
+                      style:  TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+class Item {
+  const Item(this.name,this.icon);
+  final String name;
+  final Icon icon;
 }
