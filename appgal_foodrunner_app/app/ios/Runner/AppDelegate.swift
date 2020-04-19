621ea5096cdc122e6ead02513e6e72354b3a336c
babyboy <bugs.bunny.shah@gmail.com>
2020-04-18 23:34:54 -0500

    switching_from_push_notifications_to_periodic_polling

--- appgal_foodrunner_app/app/ios/Runner/AppDelegate.swift
+++ appgal_foodrunner_app/app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +11,35 @@ import GoogleMaps
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     GMSServices.provideAPIKey("AIzaSyAAGiFi2Vm2__zWssAL6sgghY5UEPTdlDs")
+ 
+    let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: false)
+
+    let content = UNMutableNotificationContent()
+    content.title = "Tutorial Reminder"
+    content.body = "Just a reminder to read your tutorial over at appcoda.com!"
+    content.sound = UNNotificationSound.default()
+
+    let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+
+    UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    UNUserNotificationCenter.current().add(request) {(error) in
+      if let error = error {
+          print("Uh oh! We had an error: \(error)")
+      }
+    }
+
+    let selectedDate = sender.date
+    let delegate = UIApplication.shared.delegate as? AppDelegate
+    delegate?.scheduleNotification(at: selectedDate)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func scheduleNotification(at date: Date) 
+  {
+    let calendar = Calendar(identifier: .gregorian)
+    let components = calendar.dateComponents(in: .current, from: date)
+    let newComponents = DateComponents(calendar: calendar, timeZone: .current, 
+    month: components.month, day: components.day, hour: components.hour, minute: components.minute)
   }
 }
--- appgal_foodrunner_app/app/lib/main.dart
+++ appgal_foodrunner_app/app/lib/main.dart
@@ -1,6 +1,7 @@
 // Copyright 2019 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'package:app/src/messaging/polling/cloudDataPoller.dart';
 import 'package:app/src/ui/login.dart';
 import 'package:flutter/material.dart';
 
@@ -14,4 +15,7 @@ void main()
 
   //Launch the App
   runApp(new Login());
+
+  CloudDataPoller cloudDataPoller = new CloudDataPoller();
+      cloudDataPoller.startPolling();
 }
