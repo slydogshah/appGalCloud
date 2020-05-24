032428e48e7557cf38567382b1b884410f544932
babyboy <bugs.bunny.shah@gmail.com>
2020-05-24 02:41:26 -0500

    refactoring

--- appgal_foodrunner_app/app/lib/src/ui/foodRunnerDestinations.dart
+++ appgal_foodrunner_app/app/lib/src/ui/foodRunnerDestinations.dart
@@ -6,6 +6,7 @@ import 'package:app/src/model/location.dart';
 import 'package:app/src/model/sourceOrg.dart';
 import 'package:app/src/rest/activeNetworkRestClient.dart';
 import 'package:app/src/ui/pickupSource.dart';
+import 'package:app/src/ui/uiFunctions.dart';
 import 'package:flutter/material.dart';
 
 import 'applicableSources.dart';
@@ -129,62 +130,7 @@ class _FoodRunnerDestinationState extends State<FoodRunnerDestination> {
           children: this.getCards(),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0, // this will be set when a new tab is tapped
-          items: [
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.home),
-              title: new Text('Home')
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.mail),
-              title: new Text('PickUp Request'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.mail),
-              title: Text('Send Food Request')
-            )
-          ],
-          onTap: (index){
-            if(index == 0)
-            {
-              print("Home");
-            }
-            else if(index == 1)
-            {
-              print("PickUp Request");
-              /*ActiveNetworkRestClient client = ActiveNetworkRestClient();
-              SourceOrg sourceOrg = new SourceOrg("test", "TEST", "testing@test.com", null);
-              PickupRequest pickupRequest = new PickupRequest(sourceOrg);
-              Future<Iterable> future = client.sendPickupRequest(pickupRequest);
-              future.then((profiles){
-                for(Map<String, dynamic> json in profiles)
-                {
-                  Profile profile = Profile.fromJson(json);
-                  print(profile.toString());
-                }
-              });*/
-              //TODO:REMOVE_MOCK_DATA
-              SourceOrg sourceOrg = new SourceOrg("microsoft", "Microsoft", "melinda_gates@microsoft.com", null);
-              List<SourceOrg> sourceOrgs = new List();
-              sourceOrgs.add(sourceOrg);
-              Navigator.push(context,MaterialPageRoute(builder: (context) => PickupSource(sourceOrgs)));  
-            }
-            else if(index == 2)
-            {
-              print("Send Food Request");
-              ActiveNetworkRestClient client = ActiveNetworkRestClient();
-              //TODO:REMOVE_MOCK_DATA
-              SourceOrg sourceOrg = new SourceOrg("microsoft", "Microsoft", "melinda_gates@microsoft.com", null);
-              FoodRequest foodRequest = new FoodRequest("id", "VEG", sourceOrg);
-              Future<String> future = client.sendFoodRequest(foodRequest);
-              future.then((jsonString){
-                    print(jsonString);
-                    Navigator.push(context,MaterialPageRoute(builder: (context) => ApplicableSources()));
-              });
-            }
-          },
-        )
+      bottomNavigationBar: UiFunctions.bottomNavigationBar(context)
     );
     return scaffold;
   }
--- appgal_foodrunner_app/app/lib/src/ui/landingScene.dart
+++ appgal_foodrunner_app/app/lib/src/ui/landingScene.dart
@@ -5,6 +5,7 @@ import 'package:app/src/model/sourceOrg.dart';
 import 'package:app/src/rest/activeNetworkRestClient.dart';
 import 'package:app/src/ui/applicableSources.dart';
 import 'package:app/src/ui/pickupSource.dart';
+import 'package:app/src/ui/uiFunctions.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -43,62 +44,7 @@ class LandingSceneState extends State<LandingScene> {
             zoom: 11.0,
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0, // this will be set when a new tab is tapped
-          items: [
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.home),
-              title: new Text('Home')
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.mail),
-              title: new Text('PickUp Request'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.mail),
-              title: Text('Send Food Request')
-            )
-          ],
-          onTap: (index){
-            if(index == 0)
-            {
-              print("Home");
-            }
-            else if(index == 1)
-            {
-              print("PickUp Request");
-              /*ActiveNetworkRestClient client = ActiveNetworkRestClient();
-              SourceOrg sourceOrg = new SourceOrg("test", "TEST", "testing@test.com", null);
-              PickupRequest pickupRequest = new PickupRequest(sourceOrg);
-              Future<Iterable> future = client.sendPickupRequest(pickupRequest);
-              future.then((profiles){
-                for(Map<String, dynamic> json in profiles)
-                {
-                  Profile profile = Profile.fromJson(json);
-                  print(profile.toString());
-                }
-              });*/
-              //TODO:REMOVE_MOCK_DATA
-              SourceOrg sourceOrg = new SourceOrg("microsoft", "Microsoft", "melinda_gates@microsoft.com", null);
-              List<SourceOrg> sourceOrgs = new List();
-              sourceOrgs.add(sourceOrg);
-              Navigator.push(context,MaterialPageRoute(builder: (context) => PickupSource(sourceOrgs))); 
-            }
-            else if(index == 2)
-            {
-              print("Send Food Request");
-              ActiveNetworkRestClient client = ActiveNetworkRestClient();
-              //TODO:REMOVE_MOCK_DATA
-              SourceOrg sourceOrg = new SourceOrg("microsoft", "Microsoft", "melinda_gates@microsoft.com", null);
-              FoodRequest foodRequest = new FoodRequest("id", "VEG", sourceOrg);
-              Future<String> future = client.sendFoodRequest(foodRequest);
-              future.then((jsonString){
-                    print(jsonString);
-                    Navigator.push(context,MaterialPageRoute(builder: (context) => ApplicableSources()));
-              });
-            }
-          },
-        )
+        bottomNavigationBar: UiFunctions.bottomNavigationBar(context)
       ),
     );
   }
