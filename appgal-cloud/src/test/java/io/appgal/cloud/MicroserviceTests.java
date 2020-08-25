016ba52ccf2e02ed373a8ab00a4332f48261d23d
babyboy <bugs.bunny.shah@gmail.com>
2020-08-25 11:33:12 -0500

    baseline

--- appgal-cloud/src/test/java/io/appgal/cloud/MicroserviceTests.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/MicroserviceTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
-public class MicroserviceTest {
+public class MicroserviceTests {
 
     @Test
     public void testHelloEndpoint() {
