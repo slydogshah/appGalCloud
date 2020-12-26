109ddb313fd69ec92451a2c2531a8eb76966d4fa
babyboy <bugs.bunny.shah@gmail.com>
2020-12-26 16:08:11 -0500

    benchmarking

--- appgal-cloud/src/main/java/io/appgal/cloud/app/endpoint/ActiveNetwork.java
+++ appgal-cloud/src/main/java/io/appgal/cloud/app/endpoint/ActiveNetwork.java
@@ -55,7 +55,9 @@ public class ActiveNetwork {
         FoodRunner foodRunner = new FoodRunner(profile, location);
         this.networkOrchestrator.enterNetwork(foodRunner);
 
-        return Response.ok().build();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("success", true);
+        return Response.ok(jsonObject.toString()).build();
     }
 
     @Path("pickUpRequest/send")
