0ee10811953baa374df119762638e8a2765138b8
babyboy <bugs.bunny.shah@gmail.com>
2021-01-15 05:06:04 -0500

    finalizing_push_notifications

--- appgal_foodrunner_app/app/lib/src/messaging/polling/cloudDataPoller.dart
+++ appgal_foodrunner_app/app/lib/src/messaging/polling/cloudDataPoller.dart
@@ -1,22 +1,53 @@
 import 'dart:async';
+import 'dart:ui';
+
 import 'package:app/src/context/activeSession.dart';
 import 'package:app/src/model/foodRunner.dart';
 import 'package:app/src/model/profile.dart';
-import 'package:app/src/model/sourceOrg.dart';
 import 'package:app/src/rest/activeNetworkRestClient.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+
+import 'package:background_fetch/background_fetch.dart';
+import 'dart:math';
 
 class CloudDataPoller
 {
+
   static void startPolling(BuildContext context)
   {
     //pollData(context);
-    const oneSec = const Duration(seconds:60);
+    /*const oneSec = const Duration(seconds:3);
     new Timer.periodic(oneSec, (Timer t) {
       print(new DateTime.now().millisecondsSinceEpoch);
       pollData(context);
-    } );
+    } );*/
+      // Configure BackgroundFetch.
+      /*BackgroundFetch.configure(BackgroundFetchConfig(
+          minimumFetchInterval: 1,
+          forceAlarmManager: false,
+          stopOnTerminate: false,
+          startOnBoot: true,
+          enableHeadless: true,
+          requiresBatteryNotLow: false,
+          requiresCharging: false,
+          requiresStorageNotLow: false,
+          requiresDeviceIdle: false,
+          requiredNetworkType: NetworkType.NONE,
+      ), _onBackgroundFetch).then((int status) {
+        print('[BackgroundFetch] configure success: $status');
+      }).catchError((e) {
+        print('[BackgroundFetch] configure ERROR: $e');
+      });*/
+
+      // Register to receive BackgroundFetch events after app is terminated.
+    // Requires {stopOnTerminate: false, enableHeadless: true}
+    BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+
+      // Schedule a "one-shot" custom-task in 10000ms.
+    // These are fairly reliable on Android (particularly with forceAlarmManager) but not iOS,
+    // where device must be powered (and delay will be throttled by the OS).
+    var rng = new Random();
+    startBackgroundTask("taskId$rng.nextInt(10000)");
   }
 
   static void pollData(BuildContext context)
@@ -30,5 +61,36 @@ class CloudDataPoller
       //Navigator.push(context, MaterialPageRoute(builder: (context) => new PickupSource(sourceOrgs)));
       print(response);
     });
+  }
+
+  static void _onBackgroundFetch(String taskId) async {
+    print("[BackgroundFetch] Event received: $taskId");
+
+    // IMPORTANT:  You must signal completion of your fetch task or the OS can punish your app
+    // for taking too long in the background.
+    BackgroundFetch.finish(taskId);
+  }
+
+  /// This "Headless Task" is run when app is terminated.
+  static void backgroundFetchHeadlessTask(String taskId) async {
+    print("[BackgroundFetch] Headless event received: $taskId");
+    DateTime timestamp = DateTime.now();
+
+    BackgroundFetch.finish(taskId);
+
+    var rng = new Random();
+    startBackgroundTask("taskId$rng.nextInt(10000)");
+  }
+
+  static void startBackgroundTask(String taskId)
+  {
+    BackgroundFetch.scheduleTask(TaskConfig(
+          taskId: taskId,
+          delay: 1000,
+          periodic: true,
+          forceAlarmManager: true,
+          stopOnTerminate: false,
+          enableHeadless: true
+    ));
   }
 }
