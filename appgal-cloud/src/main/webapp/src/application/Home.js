931aefd4cbd53b09465ca4df159a7a47edbb945d
babyboy <bugs.bunny.shah@gmail.com>
2021-01-17 12:09:20 -0500

    schedule_pickup_flow_getting_started

--- appgal-cloud/src/main/webapp/src/application/Home.js
+++ appgal-cloud/src/main/webapp/src/application/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, createRef } from 'react'
+import ReactDOM from 'react-dom';
 import {
   CCardGroup,
   CCardFooter,
@@ -18,11 +19,24 @@ import {
   CDropdown,
   CDropdownMenu,
   CDropdownToggle,
-  CDropdownItem
+  CDropdownItem,
+  CAlert,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CFormGroup,
+  CLabel,
+  CInput,
+  CSelect
 } from '@coreui/react'
 import WidgetsBrand from './WidgetsBrand'
 import WidgetsDropdown from './WidgetsDropdown'
 import Dash from './Dash'
+import Modals from '../views/notifications/modals/Modals'
 
 import classNames from 'classnames'
 /*import {
@@ -44,12 +58,91 @@ import CIcon from '@coreui/icons-react'
 
 function schedulePickup()
 {
-    alert("Bhenchod");
+    const element = (
+                     <CModal
+                       size="sm"
+                       show="true"
+                       color="success"
+                     >
+                       <CModalHeader closeButton>
+                         <CModalTitle>Schedule A Pickup</CModalTitle>
+                       </CModalHeader>
+                       <CModalBody>
+                            <CCard>
+                                        <CCardBody>
+                                          <CRow>
+                                            <CCol xs="12">
+                                              <CFormGroup>
+                                                <CLabel htmlFor="name">Name</CLabel>
+                                                <CInput id="name" placeholder="Enter your name" required />
+                                              </CFormGroup>
+                                            </CCol>
+                                          </CRow>
+                                          <CRow>
+                                            <CCol xs="12">
+                                              <CFormGroup>
+                                                <CLabel htmlFor="ccnumber">Credit Card Number</CLabel>
+                                                <CInput id="ccnumber" placeholder="0000 0000 0000 0000" required />
+                                              </CFormGroup>
+                                            </CCol>
+                                          </CRow>
+                                          <CRow>
+                                            <CCol xs="4">
+                                              <CFormGroup>
+                                                <CLabel htmlFor="ccmonth">Month</CLabel>
+                                                <CSelect custom name="ccmonth" id="ccmonth">
+                                                  <option value="1">1</option>
+                                                  <option value="2">2</option>
+                                                  <option value="3">3</option>
+                                                  <option value="4">4</option>
+                                                  <option value="5">5</option>
+                                                  <option value="6">6</option>
+                                                  <option value="7">7</option>
+                                                  <option value="8">8</option>
+                                                  <option value="9">9</option>
+                                                  <option value="10">10</option>
+                                                  <option value="11">11</option>
+                                                  <option value="12">12</option>
+                                                </CSelect>
+                                              </CFormGroup>
+                                            </CCol>
+                                            <CCol xs="4">
+                                              <CFormGroup>
+                                                <CLabel htmlFor="ccyear">Year</CLabel>
+                                                <CSelect custom name="ccyear" id="ccyear">
+                                                  <option>2017</option>
+                                                  <option>2018</option>
+                                                  <option>2019</option>
+                                                  <option>2020</option>
+                                                  <option>2021</option>
+                                                  <option>2022</option>
+                                                  <option>2023</option>
+                                                  <option>2024</option>
+                                                  <option>2025</option>
+                                                  <option>2026</option>
+                                                </CSelect>
+                                              </CFormGroup>
+                                            </CCol>
+                                            <CCol xs="4">
+                                              <CFormGroup>
+                                                <CLabel htmlFor="cvv">CVV/CVC</CLabel>
+                                                <CInput id="cvv" placeholder="123" required/>
+                                              </CFormGroup>
+                                            </CCol>
+                                          </CRow>
+                                        </CCardBody>
+                                      </CCard>
+                       </CModalBody>
+                     </CModal>
+                 );
+   ReactDOM.unmountComponentAtNode(document.getElementById('schedulePickup'));
+   ReactDOM.render(element,document.getElementById('schedulePickup'));
 }
 
 const Widgets = () => {
   return (
       <>
+      <div id="schedulePickup"></div>
       <CRow>
       <CCol>
       <CCardGroup className="mb-4">
@@ -69,16 +162,16 @@ const Widgets = () => {
                          />
                        }
                      >
-                       <CDropdown>
-                         <CDropdownToggle color="transparent">
-                           <CIcon name="cil-settings"/>
-                         </CDropdownToggle>
-                         <CDropdownMenu className="pt-0" placement="bottom-end">
-                           <CDropdownItem onClick={schedulePickup}>Schedule</CDropdownItem>
-                           <CDropdownItem onClick={schedulePickup}>History</CDropdownItem>
-                         </CDropdownMenu>
-                       </CDropdown>
-                     </CWidgetDropdown>
+                   <CDropdown>
+                     <CDropdownToggle color="transparent">
+                       <CIcon name="cil-settings"/>
+                     </CDropdownToggle>
+                     <CDropdownMenu className="pt-0" placement="bottom-end">
+                       <CDropdownItem onClick={schedulePickup}>Schedule</CDropdownItem>
+                       <CDropdownItem onClick={schedulePickup}>History</CDropdownItem>
+                     </CDropdownMenu>
+                   </CDropdown>
+                 </CWidgetDropdown>
       </CCardGroup>
       </CCol>
       </CRow>
