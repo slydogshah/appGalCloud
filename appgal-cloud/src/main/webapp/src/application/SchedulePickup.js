0fc6f1cdd2c64d706e3240bf7359af7e8c96b8c5
babyboy <bugs.bunny.shah@gmail.com>
2021-01-23 13:52:41 -0500

    food_recovery_orchestrator_end_to_end_dashboard_to_cloud

--- appgal-cloud/src/main/webapp/src/application/SchedulePickup.js
+++ appgal-cloud/src/main/webapp/src/application/SchedulePickup.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import {
   CBadge,
   CCard,
@@ -44,24 +45,9 @@ import { DocsLink } from 'src/reusable'
 
 import usersData from 'src/views/users/UsersData'
 
-const getBadge = status => {
-  switch (status) {
-    case 'Active': return 'success'
-    case 'Inactive': return 'secondary'
-    case 'Pending': return 'warning'
-    case 'Banned': return 'danger'
-    default: return 'primary'
-  }
-}
-const fields = ['name','registered', 'role', 'status']
 
-
-const sourceOrgFields = [{key:'orgId',label:'Org'},
-{key:'orgName',label:'Name'},
-{key:'orgContactEmail',label:'Contact'}]
-
-const DroppOfOrgsView = ({dropOffOrgs}) => {
-    const components = []
+class DropOffOrgsView extends React.Component {
+    /*const components = []
     for (const [index, value] of dropOffOrgs.entries()) {
         components.push(
              <CRow>
@@ -88,7 +74,7 @@ const DroppOfOrgsView = ({dropOffOrgs}) => {
                                         <CIcon name="cil-settings"/>
                                       </CDropdownToggle>
                                       <CDropdownMenu className="pt-0" placement="bottom-end">
-                                        <CDropdownItem onClick="">Confirm</CDropdownItem>
+                                        <CDropdownItem onClick={this.handleConfirm}>Confirm</CDropdownItem>
                                       </CDropdownMenu>
                                     </CDropdown>
                                   </CWidgetDropdown>
@@ -101,52 +87,85 @@ const DroppOfOrgsView = ({dropOffOrgs}) => {
         <div>
             {components}
         </div>
-    )
+    )*/
+
+    constructor(props) {
+        super(props);
+        console.log("DropOffOrgsView : "+JSON.stringify(this.props));
+        this.handleConfirm = this.handleConfirm.bind(this);
+    }
+
+    handleConfirm(event)
+    {
+      const apiUrl = 'http://localhost:8080/notification/schedulePickup/';
+      axios.post(apiUrl,{orgId: 'microsoft'}).then((response) => {
+        this.props.history.push({
+          pathname: "/dashboard",
+          state: response.data
+        });
+      });
+    }
+
+    render()
+    {
+          const components = []
+              for (const [index, value] of this.props.dropOffOrgs.entries()) {
+                  components.push(
+                       <CRow>
+                                 <CCol>
+                                 <CCardGroup className="mb-4">
+                                        <CWidgetDropdown
+                                                  color="gradient-primary"
+                                                  header={value.orgName}
+                                                  text="Schedule Pickup"
+                                                  footerSlot={
+                                                    <ChartLineSimple
+                                                      pointed
+                                                      className="c-chart-wrapper mt-3 mx-3"
+                                                      style={{height: '70px'}}
+                                                      dataPoints={[65, 59, 84, 84, 51, 55, 40]}
+                                                      pointHoverBackgroundColor="primary"
+                                                      label="Members"
+                                                      labels="months"
+                                                    />
+                                                  }
+                                                >
+                                              <CDropdown>
+                                                <CDropdownToggle color="transparent">
+                                                  <CIcon name="cil-settings"/>
+                                                </CDropdownToggle>
+                                                <CDropdownMenu className="pt-0" placement="bottom-end">
+                                                  <CDropdownItem onClick={this.handleConfirm}>Confirm</CDropdownItem>
+                                                </CDropdownMenu>
+                                              </CDropdown>
+                                            </CWidgetDropdown>
+                                 </CCardGroup>
+                                 </CCol>
+                                 </CRow>
+                   )
+              }
+              return(
+                  <div>
+                      {components}
+                  </div>
+              )
+    }
 }
 
 class SchedulePickup extends React.Component
 {
     constructor(props) {
         super(props);
-        console.log("Constructor: "+JSON.stringify(this.props.location.state));
+        console.log("SchedulePickup: "+JSON.stringify(this.props.location.state));
     }
+
     render() {
       return (
         <>
-          <DroppOfOrgsView dropOffOrgs={this.props.location.state.dropOffOrgs} />
+          <DropOffOrgsView dropOffOrgs={this.props.location.state.dropOffOrgs} history={this.props.history}/>
         </>
       )
     }
 }
 
 export default SchedulePickup
-
-/*
-<CRow>
-              <CCol xs="12" md="4">
-                        <CCard>
-                          <CCardHeader>
-                            Schedule Pickup
-                          </CCardHeader>
-                          <CCardBody>
-                            <CForm action="" method="post" className="form-horizontal">
-                              <CFormGroup row>
-                                <CCol md="12">
-                                  <CInputGroup>
-                                    <CInputGroupPrepend>
-                                      <CInputGroupText>
-                                        <CIcon name="cil-user" />
-                                      </CInputGroupText>
-                                    </CInputGroupPrepend>
-                                    <CLabel htmlFor="ccmonth">{value.orgName}</CLabel>
-                                  </CInputGroup>
-                                </CCol>
-                              </CFormGroup>
-                            </CForm>
-                          </CCardBody>
-                          <CCardFooter>
-                            <CButton type="submit" size="sm" color="success"><CIcon name="cil-scrubber" /> Submit</CButton>
-                          </CCardFooter>
-                        </CCard>
-                      </CCol>
-           </CRow>*/
