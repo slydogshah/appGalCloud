10242e7c31aeb503cdb9a0d0580211774ac83ae8
babyboy <bugs.bunny.shah@gmail.com>
2021-02-18 09:36:53 -0500

    LocationService_getting_started

--- appgal-cloud/src/main/java/io/appgal/cloud/network/endpoint/LocationAPI.java
+++ appgal-cloud/src/main/java/io/appgal/cloud/network/endpoint/LocationAPI.java
@@ -1,10 +1,37 @@
 package io.appgal.cloud.network.endpoint;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import io.appgal.cloud.model.FoodRunner;
+import io.appgal.cloud.network.services.LocationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("location")
 public class LocationAPI {
     private static Logger logger = LoggerFactory.getLogger(LocationAPI.class);
 
+    @Inject
+    private LocationService locationService;
 
+    @Path("update")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response receiveUpdate(@RequestBody String jsonBody)
+    {
+        FoodRunner foodRunner = FoodRunner.parse(jsonBody);
+        this.locationService.receiveUpdate(foodRunner);
+
+        JsonObject responseJson = new JsonObject();
+        responseJson.addProperty("success",true);
+        return Response.ok(responseJson.toString()).build();
+    }
 }
---
+++ appgal-cloud/src/test/java/io/appgal/cloud/network/endpoint/LocationAPITest.java
@@ -0,0 +1,57 @@
+package io.appgal.cloud.network.endpoint;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import io.appgal.cloud.infrastructure.MongoDBJsonStore;
+import io.appgal.cloud.model.*;
+import io.appgal.cloud.network.services.LocationService;
+import io.appgal.cloud.network.services.NetworkOrchestrator;
+import io.appgal.cloud.util.JsonUtil;
+import io.bugsbunny.test.components.BaseTest;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+public class LocationAPITest extends BaseTest {
+    private static Logger logger = LoggerFactory.getLogger(LocationAPITest.class);
+
+    @Inject
+    private LocationService locationService;
+
+    @Inject
+    private ActiveNetwork activeNetwork;
+
+    @Inject
+    private NetworkOrchestrator networkOrchestrator;
+
+    @Test
+    public void testReceiveUpdate() throws Exception
+    {
+        double startLatitude = 30.25860595703125d;
+        double startLongitude = -97.74873352050781d;
+        Profile profile = new Profile(UUID.randomUUID().toString(), "bugs.bunny.shah@gmail.com", 8675309l, "", "", ProfileType.FOOD_RUNNER);
+        Location location = new Location(startLatitude, startLongitude);
+        FoodRunner foodRunner = new FoodRunner(profile, location);
+
+        Response response = given().when().body(foodRunner.toString()).post("/location/update/")
+                .andReturn();
+        logger.info(response.asPrettyString());
+    }
+}
