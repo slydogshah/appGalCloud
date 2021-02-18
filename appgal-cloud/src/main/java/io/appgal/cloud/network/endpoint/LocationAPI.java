fe96bee9618257eb3b83646ff389d4cb4a48e7d5
babyboy <bugs.bunny.shah@gmail.com>
2021-02-18 15:04:06 -0500

    LocationService_getting_started

--- appgal-cloud/src/main/java/io/appgal/cloud/network/endpoint/LocationAPI.java
+++ appgal-cloud/src/main/java/io/appgal/cloud/network/endpoint/LocationAPI.java
@@ -1,7 +1,6 @@
 package io.appgal.cloud.network.endpoint;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import io.appgal.cloud.model.FoodRunner;
 import io.appgal.cloud.model.Location;
 import io.appgal.cloud.network.services.LocationService;
