8a4f5428451c5c4b0d70afde85fb96f1b63f391a
babyboy <bugs.bunny.shah@gmail.com>
2021-02-25 11:14:01 -0600

    dynamic_dropoff_offline_community_draft1

---
+++ appgal-cloud/src/main/java/io/appgal/cloud/network/services/DynamicDropOffOrchestrator.java
@@ -0,0 +1,16 @@
+package io.appgal.cloud.network.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class DynamicDropOffOrchestrator {
+    private static Logger logger = LoggerFactory.getLogger(DynamicDropOffOrchestrator.class);
+
+    @Inject
+    private NetworkOrchestrator networkOrchestrator;
+
+}
