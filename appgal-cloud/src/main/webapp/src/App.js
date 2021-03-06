dcb67db591d0ffc922ff8982e58a341ac2c6d59f
babyboy <bugs.bunny.shah@gmail.com>
2021-03-06 15:56:40 -0600

    package_into_the_container

--- appgal-cloud/src/main/webapp/src/App.js
+++ appgal-cloud/src/main/webapp/src/App.js
@@ -13,9 +13,6 @@ const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
 // Pages
 const Login = React.lazy(() => import('./application/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
 
 class App extends Component {
   render() {
--- appgal-cloud/src/main/webapp/src/index.js
+++ appgal-cloud/src/main/webapp/src/index.js
@@ -5,8 +5,8 @@ import 'core-js'
 import './polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './application/Home'
 
 import { icons } from './assets/icons'
 
@@ -17,7 +17,7 @@ React.icons = icons
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Home />
   </Provider>,
   document.getElementById('root')
 )
