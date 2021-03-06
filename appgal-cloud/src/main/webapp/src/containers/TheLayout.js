e2acf9ef585134fe70163319b5337589432a8e2b
babyboy <bugs.bunny.shah@gmail.com>
2021-03-06 15:07:07 -0600

    dashboard_theme

--- appgal-cloud/src/main/webapp/src/containers/TheLayout.js
+++ appgal-cloud/src/main/webapp/src/containers/TheLayout.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import {
-  TheContent,
-  TheFooter,
-  TheHeader
-} from './index'
+import { TheContent, TheFooter, TheHeader } from './index'
 
 const TheLayout = () => {
-
   return (
     <div className="c-app c-default-layout">
       <div className="c-wrapper">
-        <TheHeader/>
-        <div className="c-body">
-          <TheContent/>
+        <TheHeader />
+        <div
+          style={{
+            background: '#a8a7c7',
+          }}
+          className="c-body"
+        >
+          <TheContent />
         </div>
-        <TheFooter/>
+        {/* <TheFooter/> */}
       </div>
     </div>
   )
