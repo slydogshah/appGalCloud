1936906f67f2e00dd99d62f5c83b9d9c391e2b07
babyboy <bugs.bunny.shah@gmail.com>
2021-03-06 18:40:23 -0600

    package_into_the_container

--- appgal-cloud/src/main/webapp/src/index.js
+++ appgal-cloud/src/main/webapp/src/index.js
@@ -5,22 +5,11 @@ import 'core-js'
 import './polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as serviceWorker from './serviceWorker'
-import './application/Home'
 
 import { icons } from './assets/icons'
-
-import { Provider } from 'react-redux'
-import store from './store'
 
 React.icons = icons
 
 ReactDOM.render(
-  <Home />
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
