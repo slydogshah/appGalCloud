884dfc3bc31be9f990089c235a14b91e30993182
babyboy <bugs.bunny.shah@gmail.com>
2021-03-19 03:07:06 -0500

    pickup_request_flow_draft1

--- appgal-cloud/src/main/webapp/src/application/AddPickupDetails.js
+++ appgal-cloud/src/main/webapp/src/application/AddPickupDetails.js
@@ -49,6 +49,14 @@ class AddPickupDetails extends React.Component
 {
     constructor(props) {
         super(props);
+        this.handleDetails = this.handleDetails.bind(this);
+    }
+
+    handleDetails(event) {
+        const apiUrl = window.location.protocol +"//"+window.location.hostname+"/notification/dropOffOrgs/?orgId=microsoft";
+        axios.get(apiUrl).then((response) => {
+              console.log(response);
+        });
     }
 
     render() {
@@ -82,13 +90,8 @@ class AddPickupDetails extends React.Component
                           </CCol>
                       </CRow>
                       <CRow>
-                        <CCol xs="6">
-                            <CButton color="primary" className="px-4">Space</CButton>
-                        </CCol>
-                      </CRow>
-                      <CRow>
                           <CCol xs="6">
-                              <CButton color="primary" className="px-4">Send</CButton>
+                              <CButton color="primary" className="px-4" onClick={this.handleDetails}>Send</CButton>
                           </CCol>
                       </CRow>
                     </CCardBody>
