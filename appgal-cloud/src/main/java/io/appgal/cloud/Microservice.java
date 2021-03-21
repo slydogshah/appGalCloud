af6f760c1a99c660937388b3743d8346dfa4513b
babyboy <bugs.bunny.shah@gmail.com>
2021-03-21 12:32:01 -0500

    seed_account

--- appgal-cloud/src/main/java/io/appgal/cloud/Microservice.java
+++ appgal-cloud/src/main/java/io/appgal/cloud/Microservice.java
@@ -8,12 +8,18 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.UUID;
 
+import io.appgal.cloud.app.services.ProfileRegistrationService;
+import io.appgal.cloud.app.services.ResourceExistsException;
+import io.appgal.cloud.model.Profile;
+import io.appgal.cloud.model.ProfileType;
 import io.bugsbunny.data.history.service.DataReplayService;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 
 @Path("/microservice")
 public class Microservice {
@@ -22,16 +28,44 @@ public class Microservice {
     @Inject
     private DataReplayService dataReplayService;
 
+    @Inject
+    private ProfileRegistrationService profileRegistrationService;
+
+    @ConfigProperty(name = "admin")
+    private String admin;
+
+    @ConfigProperty(name = "password")
+    private String password;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String hello()
+    public Response hello()
     {
-        JsonObject jsonObject = new JsonObject();
+        try {
+            Profile profile = new Profile();
+            profile.setEmail(this.admin);
+            profile.setPassword(this.password);
+            profile.setMobile(123);
+            profile.setProfileType(ProfileType.FOOD_RUNNER);
+            this.profileRegistrationService.register(profile);
 
-        jsonObject.addProperty("product", "#Jen Network");
-        jsonObject.addProperty("oid", UUID.randomUUID().toString());
-        jsonObject.addProperty("message", "HELLO_TO_HUMANITY");
+            JsonObject jsonObject = new JsonObject();
 
-        return jsonObject.toString();
+            jsonObject.addProperty("product", "#Jen Network");
+            jsonObject.addProperty("oid", UUID.randomUUID().toString());
+            jsonObject.addProperty("message", "HELLO_TO_HUMANITY");
+
+            return Response.ok(jsonObject.toString()).build();
+        }
+        catch(ResourceExistsException rxe)
+        {
+            logger.error(rxe.getMessage());
+            return Response.status(409).build();
+        }
+        catch (Exception e)
+        {
+            logger.error(e.getMessage(), e);
+            return Response.status(500).build();
+        }
     }
 }
