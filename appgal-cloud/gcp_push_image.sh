9ea4e810070e947d916ad45673770a67eb6ec9fe
babyboy <bugs.bunny.shah@gmail.com>
2021-04-05 02:40:43 -0500

    dash_to_app_and_back_orchestration_testing

--- appgal-cloud/gcp_push_image.sh
+++
@@ -1,3 +0,0 @@
-#GCP Deployment
-mvn clean package -DskipTests
-gcloud builds submit --tag gcr.io/appgallabs-271922/appgal-cloud:v1
--- appgal-cloud/gcp_qa_deploy.sh
+++
@@ -1,2 +0,0 @@
-#GCP Deployment
-gcloud run deploy --image gcr.io/appgallabs-271922/appgal-cloud:v1 --platform managed
