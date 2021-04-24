35236d3cd821267d73c6b3939a803f46cfb28ee1
babyboy <bugs.bunny.shah@gmail.com>
2021-04-24 17:43:23 -0500

    ui_pretty

--- jennetapp/lib/src/ui/foodRunner.dart
+++ jennetapp/lib/src/ui/foodRunner.dart
@@ -1,5 +1,4 @@
 import 'package:app/hotel_booking/hotel_app_theme.dart';
-import 'package:app/hotel_booking/model/hotel_list_data.dart';
 
 import 'package:app/src/background/locationUpdater.dart';
 import 'package:app/src/context/activeSession.dart';
@@ -27,8 +26,6 @@ class FoodRunnerMainScene extends StatefulWidget {
 class _FoodRunnerMainState extends State<FoodRunnerMainScene> with TickerProviderStateMixin {
 
   AnimationController animationController;
-  List<HotelListData> hotelList = HotelListData.hotelList;
-
   List<FoodRecoveryTransaction> recoveryTxs;
 
   final ScrollController _scrollController = ScrollController();
@@ -49,6 +46,23 @@ class _FoodRunnerMainState extends State<FoodRunnerMainScene> with TickerProvide
 
   @override
   Widget build(BuildContext context) {
+    if(this.recoveryTxs == null || this.recoveryTxs.isEmpty)
+    {
+        return MaterialApp(
+          title: 'Welcome to Flutter',
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text('Welcome to Flutter'),
+            ),
+            body: Center(
+              child: Text('Hello World'),
+            ),
+          ),
+        );
+    }
+
+
+
     Color primaryColor = Color(0xFF383EDB);
     Color backgroundColor = Color(0xFF383EDB);
     return Theme(
@@ -79,37 +93,14 @@ class _FoodRunnerMainState extends State<FoodRunnerMainScene> with TickerProvide
                         body: Container(
                           color: primaryColor,
                           //color: Colors.pink,
-                          child: /*ListView.builder(
-                            itemCount: this.recoveryTxs.length,
-                            padding: const EdgeInsets.only(top: 8),
-                            scrollDirection: Axis.vertical,
-                            itemBuilder: (BuildContext context, int index) {
-                              final int count = this.recoveryTxs.length;
-                              final Animation<double> animation =
-                              Tween<double>(begin: 0.0, end: 1.0).animate(
-                                  CurvedAnimation(
-                                      parent: animationController,
-                                      curve: Interval(
-                                          (1 / count) * index, 1.0,
-                                          curve: Curves.fastOutSlowIn)));
-                              animationController.forward();
-                              return RequestView(
-                                callback: () {},
-                                recoveryTransactions: this.recoveryTxs,
-                                animation: animation,
-                                index: index,
-                                hotelData: hotelList[index],
-                                animationController: animationController,
-                              );
-                            },
-                          ),*/
-                          ListView.builder(
-                            itemCount: hotelList.length,
+                          child: ListView.builder(
+                            itemCount: recoveryTxs.length,
                             padding: const EdgeInsets.only(top: 8),
                             scrollDirection: Axis.vertical,
                             itemBuilder: (BuildContext context, int index) {
                               final int count =
-                              hotelList.length > 10 ? 10 : hotelList.length;
+                              recoveryTxs.length > 10 ? 10 : recoveryTxs.length;
+                              //final int count = 1;
                               final Animation<double> animation =
                               Tween<double>(begin: 0.0, end: 1.0).animate(
                                   CurvedAnimation(
@@ -120,10 +111,10 @@ class _FoodRunnerMainState extends State<FoodRunnerMainScene> with TickerProvide
                               animationController.forward();
                               return PickUpListView(
                                 callback: () {},
-                                hotelData: hotelList[index],
                                 animation: animation,
                                 animationController: animationController,
                               );
+                              //return new PickUpListView(() {},animationController, animation);
                             },
                           ),
                         ),
@@ -234,127 +225,27 @@ class _FoodRunnerMainState extends State<FoodRunnerMainScene> with TickerProvide
   }
 }
 
-class NoActiveRequests extends StatelessWidget
-{
-
-  const NoActiveRequests(
-      {Key key,
-        this.animationController,
-        this.animation})
-      : super(key: key);
-
-  final AnimationController animationController;
-  final Animation<dynamic> animation;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animationController,
-      builder: (BuildContext context, Widget child) {
-        return FadeTransition(
-          opacity: animation,
-          child: Transform(
-            transform: Matrix4.translationValues(
-                0.0, 50 * (1.0 - animation.value), 0.0),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 24, right: 24, top: 8, bottom: 16),
-              child: InkWell(
-                splashColor: Colors.transparent,
-                onTap: () {
-                  //callback();
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.6),
-                        offset: const Offset(4, 4),
-                        blurRadius: 16,
-                      ),
-                    ],
-                  ),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                    child: Stack(
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Container(
-                              color: HotelAppTheme.buildLightTheme()
-                                  .backgroundColor,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Expanded(
-                                    child: Container(
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 16, top: 8, bottom: 8),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        Positioned(
-                          top: 8,
-                          right: 8,
-                          child: Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              borderRadius: const BorderRadius.all(
-                                Radius.circular(32.0),
-                              ),
-                              onTap: () {},
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                /*child: Icon(
-                                  Icons.favorite_border,
-                                  color: HotelAppTheme.buildLightTheme()
-                                      .primaryColor,
-                                ),*/
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-
 class PickUpListView extends StatelessWidget {
+  /*PickUpListView(VoidCallback callback,AnimationController animationController,
+      Animation<dynamic> animation)
+  {
+    this.callback = callback;
+    this.animationController = animationController;
+    this.animation = animation;
+  }
+
+  VoidCallback callback;
+  AnimationController animationController;
+  Animation<dynamic> animation;*/
+
   const PickUpListView(
       {Key key,
-        this.hotelData,
         this.animationController,
         this.animation,
         this.callback})
       : super(key: key);
 
   final VoidCallback callback;
-  final HotelListData hotelData;
   final AnimationController animationController;
   final Animation<dynamic> animation;
 
@@ -396,7 +287,7 @@ class PickUpListView extends StatelessWidget {
                             AspectRatio(
                               aspectRatio: 2,
                               child: Image.asset(
-                                hotelData.imagePath,
+                                'assets/hotel/hotel_1.png',
                                 fit: BoxFit.cover,
                               ),
                             ),
@@ -419,7 +310,7 @@ class PickUpListView extends StatelessWidget {
                                           CrossAxisAlignment.start,
                                           children: <Widget>[
                                             Text(
-                                              hotelData.titleTxt,
+                                              "church.com",
                                               textAlign: TextAlign.left,
                                               style: TextStyle(
                                                 fontWeight: FontWeight.w600,
@@ -433,7 +324,7 @@ class PickUpListView extends StatelessWidget {
                                               MainAxisAlignment.start,
                                               children: <Widget>[
                                                 Text(
-                                                  hotelData.subTxt,
+                                                  "blah.com",
                                                   style: TextStyle(
                                                       fontSize: 14,
                                                       color: Colors.grey
@@ -449,7 +340,7 @@ class PickUpListView extends StatelessWidget {
                                                       .buildLightTheme()
                                                       .primaryColor,
                                                 ),
-                                                Expanded(
+                                                /*Expanded(
                                                   child: Text(
                                                     '${hotelData.dist.toStringAsFixed(1)} km to city',
                                                     overflow:
@@ -459,7 +350,7 @@ class PickUpListView extends StatelessWidget {
                                                         color: Colors.grey
                                                             .withOpacity(0.8)),
                                                   ),
-                                                ),
+                                                ),*/
                                               ],
                                             ),
                                             Padding(
@@ -467,7 +358,7 @@ class PickUpListView extends StatelessWidget {
                                               const EdgeInsets.only(top: 4),
                                               child: Row(
                                                 children: <Widget>[
-                                                  SmoothStarRating(
+                                                  /*SmoothStarRating(
                                                     allowHalfRating: true,
                                                     starCount: 5,
                                                     rating: hotelData.rating,
@@ -485,7 +376,7 @@ class PickUpListView extends StatelessWidget {
                                                         fontSize: 14,
                                                         color: Colors.grey
                                                             .withOpacity(0.8)),
-                                                  ),
+                                                  ),*/
                                                 ],
                                               ),
                                             ),
@@ -504,7 +395,7 @@ class PickUpListView extends StatelessWidget {
                                       CrossAxisAlignment.end,
                                       children: <Widget>[
                                         Text(
-                                          '\$${hotelData.perNight}',
+                                          'foo',
                                           textAlign: TextAlign.left,
                                           style: TextStyle(
                                             fontWeight: FontWeight.w600,
