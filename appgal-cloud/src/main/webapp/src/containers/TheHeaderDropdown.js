0ac0ab29c7824159e0757396bacdbe8a93b0c7a9
babyboy <bugs.bunny.shah@gmail.com>
2021-05-12 17:10:19 -0500

    dash_ui_tweaks

--- appgal-cloud/src/main/webapp/src/containers/TheHeaderDropdown.js
+++ appgal-cloud/src/main/webapp/src/containers/TheHeaderDropdown.js
@@ -13,47 +13,123 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Hidden from "@material-ui/core/Hidden";
+import Poppers from "@material-ui/core/Popper";
+import Divider from "@material-ui/core/Divider";
+// @material-ui/icons
+import Person from "@material-ui/icons/Person";
+import Notifications from "@material-ui/icons/Notifications";
+import Dashboard from "@material-ui/icons/Dashboard";
+import Search from "@material-ui/icons/Search";
+// core components
+import CustomInput from "../components/CustomInput/CustomInput.js";
+import Button from "../components/CustomButtons/Button.js";
+
+import styles from "../assets/jss/material-dashboard-react/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
 //TODO: hook the actual logout routine
 
-class TheHeaderDropdown extends React.Component {
-   constructor(props) {
-       super(props);
-   }
 
-  render(){
+
+//class TheHeaderDropdown extends React.Component {
+export default function AdminNavbarLinks() {
+        const classes = useStyles();
+        const [openNotification, setOpenNotification] = React.useState(null);
+        const [openProfile, setOpenProfile] = React.useState(null);
+        const handleClickNotification = event => {
+          if (openNotification && openNotification.contains(event.target)) {
+            setOpenNotification(null);
+          } else {
+            setOpenNotification(event.currentTarget);
+          }
+        };
+        const handleCloseNotification = () => {
+          setOpenNotification(null);
+        };
+        const handleClickProfile = event => {
+          if (openProfile && openProfile.contains(event.target)) {
+            setOpenProfile(null);
+          } else {
+            setOpenProfile(event.currentTarget);
+          }
+        };
+        const handleCloseProfile = () => {
+          setOpenProfile(null);
+        };
       return (
-        <CDropdown
-          inNav
-          className="c-header-nav-items mx-2"
-          direction="down"
-        >
-          <CDropdownToggle className="c-header-nav-link" caret={false}>
-            <div className="c-avatar">
-              <CImg
-                src={'images/dropdown-arrow.svg'}
-                className="c-avatar-img"
-                alt="options"
-              />
-            </div>
-          </CDropdownToggle>
-          <CDropdownMenu className="pt-0" placement="bottom-end">
-            <CDropdownItem>
-              <CIcon name="cil-lock-locked" className="mfe-2" />
-              <CLink to="/home" className="btn btn-primary">Home</CLink>
-            </CDropdownItem>
-            <CDropdownItem>
-              <CIcon name="cil-user" className="mfe-2" />
-              <CLink to="/profile" className="btn btn-primary">Profile</CLink>
-            </CDropdownItem>
-            <CDropdownItem divider />
-            <CDropdownItem>
-              <CIcon name="cil-lock-locked" className="mfe-2" />
-              <CLink to="/" className="btn btn-primary">Logout</CLink>
-            </CDropdownItem>
-          </CDropdownMenu>
-        </CDropdown>
+        <div className={classes.manager}>
+                <Button
+                  color={window.innerWidth > 959 ? "transparent" : "white"}
+                  justIcon={window.innerWidth > 959}
+                  simple={!(window.innerWidth > 959)}
+                  aria-owns={openProfile ? "profile-menu-list-grow" : null}
+                  aria-haspopup="true"
+                  onClick={handleClickProfile}
+                  className={classes.buttonLink}
+                >
+                  <Person className={classes.icons} />
+                  <Hidden mdUp implementation="css">
+                    <p className={classes.linkText}>Profile</p>
+                  </Hidden>
+                </Button>
+                <Poppers
+                  open={Boolean(openProfile)}
+                  anchorEl={openProfile}
+                  transition
+                  disablePortal
+                  className={
+                    classNames({ [classes.popperClose]: !openProfile }) +
+                    " " +
+                    classes.popperNav
+                  }
+                >
+                  {({ TransitionProps, placement }) => (
+                    <Grow
+                      {...TransitionProps}
+                      id="profile-menu-list-grow"
+                      style={{
+                        transformOrigin:
+                          placement === "bottom" ? "center top" : "center bottom"
+                      }}
+                    >
+                      <Paper>
+                        <ClickAwayListener onClickAway={handleCloseProfile}>
+                          <MenuList role="menu">
+                            <MenuItem
+                              onClick={handleCloseProfile}
+                              className={classes.dropdownItem}
+                            >
+                              <CLink to="/home" className={classes.dropdownItem}>Home</CLink>
+                            </MenuItem>
+                            <MenuItem
+                              onClick={handleCloseProfile}
+                              className={classes.dropdownItem}
+                            >
+                              <CLink to="/profile" className={classes.dropdownItem}>Account</CLink>
+                            </MenuItem>
+                            <Divider light />
+                            <MenuItem
+                              onClick={handleCloseProfile}
+                              className={classes.dropdownItem}
+                            >
+                              <CLink to="/" className={classes.dropdownItem}>Logout</CLink>
+                            </MenuItem>
+                          </MenuList>
+                        </ClickAwayListener>
+                      </Paper>
+                    </Grow>
+                  )}
+                </Poppers>
+           </div>
       );
   }
-}
-
-export default TheHeaderDropdown
