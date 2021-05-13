90a85cfb4396c4156eb0010801191a9dee1cf4fd
babyboy <bugs.bunny.shah@gmail.com>
2021-05-12 19:45:24 -0500

    dash_ui_tweaks

--- appgal-cloud/src/main/webapp/src/application/Home.js
+++ appgal-cloud/src/main/webapp/src/application/Home.js
@@ -133,166 +133,128 @@ function PickupTasks(props) {
 }
 
 
+
 const WaitOnData = ({state, props, handlePickup, handlePickupHistory}) => {
-    if (state.data === null) {
+    /*if (state.data === null) {
       return <p>Loading...</p>;
-    }
-
-    const inProgress = state.data.pending;
-    var array = [];
-    var tasksIndexes = [];
-    for (const [index, value] of inProgress.entries()) {
-        const org = value.pickupNotification.sourceOrg.orgName;
-        array.push(org);
-        tasksIndexes.push(index);
-    }
-
-    const history = state.data.history;
-    var historyArray = [];
-    var historyIndexes = [];
-    for (const [index, value] of history.entries()) {
-        const org = value.pickupNotification.sourceOrg.orgName;
-        historyArray.push(org);
-        historyIndexes.push(index);
-    }
-    return (
-          <>
-                    <br/><br/><br/><br/>
-                    <div id="schedulePickup"></div>
-                    <CRow>
-                    <CCol>
-                    <CCardGroup className="mb-4">
-                           <CWidgetDropdown
-                                     color="gradient-primary"
-                                     header={state.data.pending.length}
-                                     text="Deliveries In-Progress"
-                                     footerSlot={
-                                       <ChartLineSimple
-                                         pointed
-                                         className="c-chart-wrapper mt-3 mx-3"
-                                         style={{height: '70px'}}
-                                         dataPoints={[65, 59, 84, 84, 51, 55, 40]}
-                                         pointHoverBackgroundColor="primary"
-                                         label="Members"
-                                         labels="months"
-                                       />
-                                     }
-                                   >
-                                   <CDropdown>
-                                      <CDropdownToggle color="transparent">
-                                        <CIcon name="cil-settings"/>
-                                      </CDropdownToggle>
-                                      <CDropdownMenu className="pt-0" placement="bottom-end">
-                                       <CDropdownItem onClick={(e) => {
-                                                props.history.push({
-                                                   pathname: "/addPickupDetails"
-                                                });
-                                       }}>Schedule</CDropdownItem>
-                                      </CDropdownMenu>
-                                   </CDropdown>
-                               </CWidgetDropdown>
-                    </CCardGroup>
-                    </CCol>
-                    </CRow>
-                    <CRow>
-                        <CCol>
-                            <GridContainer>
-                                                         <GridItem xs={12} sm={12} md={6}>
-                                                           <CustomTabs
-                                                             title="Delivery Status"
-                                                             headerColor="primary"
-                                                             tabs={[
-                                                               {
-                                                                 tabName: "In-Progress",
-                                                                 tabIcon: BugReport,
-                                                                 tabContent: (
-                                                                   <PickupTasks
-                                                                     checkedIndexes={[0]}
-                                                                     tasksIndexes={tasksIndexes}
-                                                                     tasks={array}
-                                                                     actions={inProgress}
-                                                                   />
-                                                                 )
-                                                               },
-                                                               {
-                                                                tabName: "History",
-                                                                tabIcon: BugReport,
-                                                                tabContent: (
-                                                                  <PickupTasks
-                                                                    checkedIndexes={[0]}
-                                                                    tasksIndexes={historyIndexes}
-                                                                    tasks={historyArray}
-                                                                    actions={[]}
-                                                                  />
-                                                                )
-                                                              },
-                                                             ]}
-                                                           />
-                                                         </GridItem>
-                                                     </GridContainer>
-                        </CCol>
-                    </CRow>
-                    </>
-        )
-}
-
-class Home extends React.Component {
-  element;
-  constructor(props) {
-      super(props);
-      this.handlePickup = this.handlePickup.bind(this);
-      this.handlePickupProcess = this.handlePickupProcess.bind(this);
-      this.handlePickupHistory = this.handlePickupHistory.bind(this);
-      this.state = {data: null};
-      this.renderMyData();
-  }
-
-  renderMyData(){
+    }*/
     const orgId = store.getState().sourceOrg.orgId;
     const apiUrl = window.location.protocol +"//"+window.location.hostname+"/tx/recovery/?orgId="+orgId;
     axios.get(apiUrl).then((response) => {
-        console.log("MY_DATA: "+JSON.stringify(response.data));
-        this.setState({data: response.data});
-    });
-  }
+        //console.log("MY_DATA: "+JSON.stringify(response.data));
+        //this.setState({data: response.data});
+        state.data = response.data;
+        //console.log("******INVOKED********");
+            const inProgress = state.data.pending;
+            var array = [];
+            var tasksIndexes = [];
+            for (const [index, value] of inProgress.entries()) {
+                const org = value.pickupNotification.sourceOrg.orgName;
+                array.push(org);
+                tasksIndexes.push(index);
+            }
 
-  handlePickup(event)
-  {
-     this.element = (
-          <CModal
-            size="sm"
-            show={true}
-            color="success"
-            fade="true"
-          >
-            <CModalHeader>
-              <CModalTitle>Schedule A Pickup</CModalTitle>
-            </CModalHeader>
-            <CModalBody>
-                 <CCard>
-                     <CCardBody>
-                       <CRow>
-                         <CCol>
-                           <CFormGroup>
-                             <CLabel htmlFor="ccmonth">Preferred Pickup Time</CLabel>
-                             <CSelect custom name="ccmonth" id="ccmonth">
-                               <option value="0">12:00 AM</option>
-                               <option value="12">12:00 PM</option>
-                               <option value="23">11:59 PM</option>
-                             </CSelect>
-                           </CFormGroup>
-                         </CCol>
-                       </CRow>
-                     </CCardBody>
-                   </CCard>
-            </CModalBody>
-            <CModalFooter>
-                <CButton color="success" onClick={this.handlePickupProcess}>Schedule</CButton>
-            </CModalFooter>
-          </CModal>
-     );
-     ReactDOM.unmountComponentAtNode(document.getElementById('schedulePickup'));
-     ReactDOM.render(this.element,document.getElementById('schedulePickup'));
+            const history = state.data.history;
+            var historyArray = [];
+            var historyIndexes = [];
+            for (const [index, value] of history.entries()) {
+                const org = value.pickupNotification.sourceOrg.orgName;
+                historyArray.push(org);
+                historyIndexes.push(index);
+            }
+            const element = (
+                  <>
+                            <br/><br/><br/><br/>
+                            <CRow>
+                            <CCol>
+                            <CCardGroup className="mb-4">
+                                   <CWidgetDropdown
+                                             color="gradient-primary"
+                                             header={state.data.pending.length}
+                                             text="Deliveries In-Progress"
+                                             footerSlot={
+                                               <ChartLineSimple
+                                                 pointed
+                                                 className="c-chart-wrapper mt-3 mx-3"
+                                                 style={{height: '70px'}}
+                                                 dataPoints={[65, 59, 84, 84, 51, 55, 40]}
+                                                 pointHoverBackgroundColor="primary"
+                                                 label="Members"
+                                                 labels="months"
+                                               />
+                                             }
+                                           >
+                                           <CDropdown>
+                                              <CDropdownToggle color="transparent">
+                                                <CIcon name="cil-settings"/>
+                                              </CDropdownToggle>
+                                              <CDropdownMenu className="pt-0" placement="bottom-end">
+                                               <CDropdownItem onClick={(e) => {
+                                                        props.history.push({
+                                                           pathname: "/addPickupDetails"
+                                                        });
+                                               }}>Schedule</CDropdownItem>
+                                              </CDropdownMenu>
+                                           </CDropdown>
+                                       </CWidgetDropdown>
+                            </CCardGroup>
+                            </CCol>
+                            </CRow>
+                            <CRow>
+                                <CCol>
+                                    <GridContainer>
+                                                                 <GridItem xs={12} sm={12} md={6}>
+                                                                   <CustomTabs
+                                                                     title="Delivery Status"
+                                                                     headerColor="primary"
+                                                                     tabs={[
+                                                                       {
+                                                                         tabName: "In-Progress",
+                                                                         tabIcon: BugReport,
+                                                                         tabContent: (
+                                                                           <PickupTasks
+                                                                             checkedIndexes={[0]}
+                                                                             tasksIndexes={tasksIndexes}
+                                                                             tasks={array}
+                                                                             actions={inProgress}
+                                                                           />
+                                                                         )
+                                                                       },
+                                                                       {
+                                                                        tabName: "History",
+                                                                        tabIcon: BugReport,
+                                                                        tabContent: (
+                                                                          <PickupTasks
+                                                                            checkedIndexes={[0]}
+                                                                            tasksIndexes={historyIndexes}
+                                                                            tasks={historyArray}
+                                                                            actions={[]}
+                                                                          />
+                                                                        )
+                                                                      },
+                                                                     ]}
+                                                                   />
+                                                                 </GridItem>
+                                                             </GridContainer>
+                                </CCol>
+                            </CRow>
+                            </>
+                );
+                ReactDOM.unmountComponentAtNode(document.getElementById('parent'));
+                ReactDOM.render(element,document.getElementById('parent'));
+    });
+
+    //TODO: add spinner
+    return <div/>;
+}
+
+class Home extends React.Component {
+  constructor(props) {
+      super(props);
+      this.handlePickupProcess = this.handlePickupProcess.bind(this);
+      this.handlePickupHistory = this.handlePickupHistory.bind(this);
+      this.state = {data: null};
+      //this.renderMyData();
   }
 
   handlePickupProcess(event)
@@ -312,7 +274,7 @@ class Home extends React.Component {
   render() {
      return (
           <>
-          <div>
+          <div id="parent">
             <WaitOnData state={this.state} props={this.props} handlePickup={this.handlePickup} handlePickupHistory={this.handlePickupHistory}/>
           </div>
           </>
