56068986c12914f33af1cb6e613e965f2bae52d3
babyboy <bugs.bunny.shah@gmail.com>
2021-06-05 02:15:06 -0500

    Offline Community Support #38

--- appgal-cloud/src/test/java/io/appgal/cloud/network/services/DynamicDropOffOrchestratorTests.java
+++ appgal-cloud/src/test/java/io/appgal/cloud/network/services/DynamicDropOffOrchestratorTests.java
@@ -1,11 +1,17 @@
 package io.appgal.cloud.network.services;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import io.appgal.cloud.app.services.ProfileRegistrationService;
+import io.appgal.cloud.infrastructure.MongoDBJsonStore;
 import io.appgal.cloud.model.*;
 import io.appgal.cloud.util.JsonUtil;
 import io.bugsbunny.test.components.BaseTest;
+import io.bugsbunny.test.components.MockData;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -18,6 +24,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -28,58 +35,46 @@ public class DynamicDropOffOrchestratorTests extends BaseTest {
     @Inject
     private DynamicDropOffOrchestrator dynamicDropOffOrchestrator;
 
+    @Inject
+    private ProfileRegistrationService profileRegistrationService;
+
+    private FoodRunner foodRunner;
+
     @BeforeEach
     public void setUp() throws Exception
     {
-        super.setUp();
-
-        OffsetDateTime start = OffsetDateTime.now(ZoneOffset.UTC).withHour(1).withMinute(0).withSecond(0);
-
-        OffsetDateTime middle = OffsetDateTime.now(ZoneOffset.UTC).withHour(12).withMinute(0).withSecond(0);
-
-        OffsetDateTime end = OffsetDateTime.now(ZoneOffset.UTC).withHour(20).withMinute(0).withSecond(0);
-
-        List<OffsetDateTime> schedulePickUpNotificationList = new LinkedList<>();
-        schedulePickUpNotificationList.add(middle);
-        schedulePickUpNotificationList.add(end);
-        schedulePickUpNotificationList.add(start);
-        logger.info(schedulePickUpNotificationList.toString());
-
-        for (OffsetDateTime cour : schedulePickUpNotificationList) {
-            SourceOrg sourceOrg = new SourceOrg("microsoft", "Microsoft", "melinda_gates@microsoft.com", true);
-            sourceOrg.setProducer(true);
-            Profile profile = new Profile(UUID.randomUUID().toString(), "bugs.bunny.shah@gmail.com", 8675309l, "", "", ProfileType.FOOD_RUNNER);
-            Location location = new Location(0.0d, 0.0d);
-            FoodRunner bugsBunny = new FoodRunner(profile, location);
-
-            SchedulePickUpNotification notification = new SchedulePickUpNotification(UUID.randomUUID().toString());
-            notification.setDropOffDynamic(true);
-            notification.setFoodRunner(bugsBunny);
-            notification.setStart(cour);
-            logger.info("********************************************");
-            JsonUtil.print(this.getClass(),notification.toJson());
-            logger.info(cour.toString() + ":" + cour.toEpochSecond());
-        }
-        Thread.sleep(5000);
+        this.foodRunner = MockData.mockFoodRunner();
+        this.foodRunner.getProfile().setEmail("jen@appgallabs.io");
+        this.profileRegistrationService.register(foodRunner.getProfile());
+        this.loginFoodRunner(foodRunner.getProfile().getEmail(),foodRunner.getProfile().getPassword());
     }
 
     @Test
-    public void notifyAvailability()
+    public void notifyAvailability() throws Exception
     {
-        this.dynamicDropOffOrchestrator.notifyAvailability("123");
+        this.dynamicDropOffOrchestrator.notifyAvailability(this.foodRunner.getProfile().getEmail());
     }
 
     @Test
-    public void testOrchestrateOfflineCommunity()
+    public void getOfflineDropOffHelpers() throws Exception
     {
-        JsonObject json = this.dynamicDropOffOrchestrator.orchestrateOfflineCommunity();
-        JsonUtil.print(this.getClass(),json);
+        this.dynamicDropOffOrchestrator.notifyAvailability(this.foodRunner.getProfile().getEmail());
+        List<FoodRunner> helpers = this.dynamicDropOffOrchestrator.getOfflineDropOffHelpers();
+        JsonUtil.print(this.getClass(),JsonParser.parseString(helpers.toString()));
     }
 
-    @Test
-    public void testGetOfflineDropOffPipeline() throws Exception
+    private JsonObject loginFoodRunner(String email,String password)
     {
-        JsonArray pipeline = this.dynamicDropOffOrchestrator.getOfflineDropOffPipeline();
-        JsonUtil.print(this.getClass(),pipeline);
+        JsonObject loginJson = new JsonObject();
+        loginJson.addProperty("email", email);
+        loginJson.addProperty("password", password);
+        loginJson.addProperty("latitude", 30.2698104d);
+        loginJson.addProperty("longitude",-97.75115579999999);
+        Response response = given().header("User-Agent","Dart").body(loginJson.toString()).when().post("/registration/login").andReturn();
+        String jsonString = response.getBody().print();
+        JsonElement responseJson = JsonParser.parseString(jsonString);
+        //JsonUtil.print(this.getClass(), responseJson);
+        assertEquals(200, response.getStatusCode());
+        return responseJson.getAsJsonObject();
     }
 }
