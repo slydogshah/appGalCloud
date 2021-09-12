0297258ffd31870f61856f8f359710e716351b67
babyboy <bugs.bunny.shah@gmail.com>
2021-09-12 18:47:39 -0500

    tweaks:cleanup

--- jennetapp/android/app/src/main/java/io/appgallabs/jennetwork/MyFirebaseMessagingService.java
+++ jennetapp/android/app/src/main/java/io/appgallabs/jennetwork/MyFirebaseMessagingService.java
@@ -1,18 +1,4 @@
-package io.appgallabs.jennetwork; /**
- * Copyright 2016 Google Inc. All Rights Reserved.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package io.appgallabs.jennetwork;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -44,27 +30,19 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import javax.net.ssl.HttpsURLConnection;
 
-
-/**
- * NOTE: There can only be one service in each app that receives FCM messages. If multiple
- * are declared in the Manifest then the first one will be chosen.
- *
- * In order to make this Java sample functional, you must remove the following from the Kotlin messaging
- * service in the AndroidManifest.xml:
- *
- * <intent-filter>
- *   <action android:name="com.google.firebase.MESSAGING_EVENT" />
- * </intent-filter>
- */
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
+    public static String pushToken;
+
+    private void sendRegistrationToServer(String token) {
+        MyFirebaseMessagingService.pushToken = token;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        //Initialize Firebase
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create channel to show notifications.
             String channelId = getString(R.string.default_notification_channel_id);
@@ -75,212 +53,47 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                     channelName, NotificationManager.IMPORTANCE_LOW));
         }
 
-        // If a notification message is tapped, any data accompanying the notification
-        // message is available in the intent extras. In this sample the launcher
-        // intent is fired when the notification is tapped, so any accompanying data would
-        // be handled here. If you want a different intent fired, set the click_action
-        // field of the notification message to the desired intent. The launcher intent
-        // is used when no click_action is specified.
-        //
-        // Handle possible data accompanying notification message.
-        // [START handle_data_extras]
-        /*if (getIntent().getExtras() != null) {
-            for (String key : getIntent().getExtras().keySet()) {
-                Object value = getIntent().getExtras().get(key);
-                Log.d(TAG, "Key: " + key + " Value: " + value);
-            }
-        }*/
-        // [END handle_data_extras]
-
-        Log.d(TAG, "Subscribing to weather topic");
-        // [START subscribe_topics]
-        FirebaseMessaging.getInstance().subscribeToTopic("weather")
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        String msg = getString(R.string.msg_subscribed);
-                        if (!task.isSuccessful()) {
-                            msg = getString(R.string.msg_subscribe_failed);
-                        }
-                        Log.d(TAG, msg);
-                    }
-                });
-        // [END subscribe_topics]
-
-        // Get token
-        // [START log_reg_token]
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(new OnCompleteListener<String>() {
                     @Override
                     public void onComplete(@NonNull Task<String> task) {
                         if (!task.isSuccessful()) {
-                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            Log.i(TAG, "Fetching FCM registration token failed", task.getException());
                             return;
                         }
 
-                        // Get new FCM registration token
                         String token = task.getResult();
-
-                        //throw new RuntimeException("FORCED");
-                        /*Thread t = new Thread(()->{
-                            registerFcmToken(token);
-                        });
-                        t.start();*/
-                        System.out.println(token);
                         sendRegistrationToServer(token);
                     }
                 });
-        // [END log_reg_token]
     }
 
-    public static String pushToken;
-    private void sendRegistrationToServer(String token) {
-        /*Intent intent = new Intent("1");
-        intent.putExtra("token",token);
-        getApplicationContext().sendBroadcast(intent);*/
-        MyFirebaseMessagingService.pushToken = token;
-    }
-
-    /*private void registerFcmToken(String fcmToken){
-        System.out.println("***********************************");
-        System.out.println(fcmToken);
-        try {
-            URL url = new URL("https://appgal-cloud-do2cwgwhja-uc.a.run.app/activeNetwork/registerPush");
-            //URL url = new URL("https://10.0.2.2/activeNetwork/registerPush");
-            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-            try {
-                urlConnection.setDoOutput(true);
-
-                String email = "jen@app.io";
-                String json = "{\"email\":"+email+",\"pushToken\":"+fcmToken;
-
-                OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-                out.write(json.getBytes(StandardCharsets.UTF_8));
-                out.flush();
-
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-
-                byte[] data = new byte[1024];
-                in.read(data);
-                System.out.println("TOKEN_REGISTRATION_SUCCESS: "+new String(data,StandardCharsets.UTF_8));
-            } finally {
-                urlConnection.disconnect();
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }*/
-
-    /**
-     * Called when message is received.
-     *
-     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
-     */
-    // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages
-        // are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data
-        // messages are the type
-        // traditionally used with GCM. Notification messages are only received here in
-        // onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated
-        // notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages
-        // containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always
-        // sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
+            if (true) {
                 scheduleJob();
             } else {
-                // Handle message within 10 seconds
                 handleNow();
             }
 
         }
-
-        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
             sendNotification(remoteMessage.getNotification().getBody());
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
-    // [END receive_message]
 
-
-    // [START on_new_token]
-    /**
-     * There are two scenarios when onNewToken is called:
-     * 1) When a new token is generated on initial app startup
-     * 2) Whenever an existing token is changed
-     * Under #2, there are three scenarios when the existing token is changed:
-     * A) App is restored to a new device
-     * B) User uninstalls/reinstalls the app
-     * C) User clears app data
-     */
     @Override
     public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
         sendRegistrationToServer(token);
     }
-    // [END on_new_token]
 
-    /**
-     * Schedule async work using WorkManager.
-     */
     private void scheduleJob() {
-        // [START dispatch_job]
-        //OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class)
-        //        .build();
-        //WorkManager.getInstance(this).beginWith(work).enqueue();
-        // [END dispatch_job]
     }
 
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     */
     private void handleNow() {
-        Log.d(TAG, "Short lived task is done.");
     }
 
-    /**
-     * Persist token to third-party servers.
-     *
-     * Modify this method to associate the user's FCM registration token with any
-     * server-side account maintained by your application.
-     *
-     * @param token The new token.
-     */
-
-
-    /**
-     * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
-     */
     private void sendNotification(String messageBody) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -301,7 +114,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        // Since android Oreo notification channel is needed.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(channelId,
                     "Channel human readable title",
