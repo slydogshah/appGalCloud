a64d4d6168fb142693615cf609f51aa706fa1423
babyboy <bugs.bunny.shah@gmail.com>
2021-10-22 16:07:18 -0400

    App: Test and make sure location updates are received as the location changes

--- jennetapp/lib/src/background/locationUpdater.dart
+++ jennetapp/lib/src/background/locationUpdater.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:app/src/context/activeSession.dart';
 import 'package:location/location.dart';
 
@@ -9,64 +8,47 @@ import 'package:app/src/rest/activeNetworkRestClient.dart';
 
 class LocationUpdater
 {
-  static String scheduledTaskId = "com.transistersoft.io.appgallabs.jen.network";
-  static String standardTaskId = "com.transistorsoft.fetch";
+  Location location;
+  bool isBackgroundModeEnabled;
+  ActiveNetworkRestClient activeNetworkClient= new ActiveNetworkRestClient();
 
-  static Location location = new Location();
-  static bool _serviceEnabled;
-  static bool isBackgroundModeEnabled;
-  static PermissionStatus _permissionGranted;
-  static LocationData _locationData;
-  static ActiveNetworkRestClient activeNetworkClient= new ActiveNetworkRestClient();
+  static LocationUpdater singleton;
 
   static void start() async{
-    //_permissionGranted = await location.requestPermission();
-    isBackgroundModeEnabled = await location.isBackgroundModeEnabled();
-    print("***********LOCATION_BACKGROUND_MODE_ENABLED**********");
-    print(isBackgroundModeEnabled);
+    if(singleton == null){
+      singleton = new LocationUpdater();
+    }
+    singleton.location = new Location();
+    singleton.isBackgroundModeEnabled =
+        await singleton.location.enableBackgroundMode(enable: true);
   }
 
   static Future<FoodRunnerLocation> getLocation() async
   {
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-    }
-
-    LocationData data = await location.getLocation();
+    LocationData data = await singleton.location.getLocation();
     FoodRunnerLocation foodRunnerLocation = new FoodRunnerLocation(data.latitude,data.longitude);
     ActiveSession.getInstance().setLocation(foodRunnerLocation);
     FoodRunnerLocation active = ActiveSession.getInstance().getLocation();
-    print("FOOD_LOCATION_RETURNED: $active");
+    //print("FOOD_LOCATION_RETURNED: $active");
     return active;
   }
 
   static void startPolling(Profile profile) async
   {
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        print("******************");
-        print("SERVICE_DISABLED");
-        return;
-      }
-    }
+    //print("***********LOCATION_BACKGROUND_MODE_ENABLED_DURING_POLLING**********");
+    //print(singleton.isBackgroundModeEnabled);
 
-    if(!isBackgroundModeEnabled) {
-      location.enableBackgroundMode(enable: true);
-    }
+    singleton.location.onLocationChanged.listen((LocationData currentLocation) {
+      //print("LOCATION_UPDATE_RECEIVED: $currentLocation.latitude,$currentLocation.longitude");
 
-    //Start receiving location updates
-    /*Future<LocationData> locationData = location.getLocation();
-      locationData.then((data){
-        Map<String,double> map = new Map();
+      // Use current location
+      Map<String,double> map = new Map();
 
-        map['latitude'] = data.latitude;
-        map['longitude'] = data.longitude;
-        LocationData location = LocationData.fromMap(map);
-        Future<String> future = activeNetworkClient.sendLocationUpdate(location);
-        future.then((result){}).catchError((error){});
-      });*/
+      map['latitude'] = currentLocation.latitude;
+      map['longitude'] = currentLocation.longitude;
+      LocationData location = LocationData.fromMap(map);
+      Future<String> future = singleton.activeNetworkClient.sendLocationUpdate(location);
+      future.then((result){}).catchError((error){});
+    });
   }
 }
