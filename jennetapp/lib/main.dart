8ce18a0021f5f669c917340bb78886276e6e3c36
babyboy <bugs.bunny.shah@gmail.com>
2021-12-01 00:01:15 -0500

    app_store_readiness

--- jennetapp/lib/main.dart
+++ jennetapp/lib/main.dart
@@ -54,7 +54,7 @@ void main(String env) {
   WidgetsFlutterBinding.ensureInitialized();
 
   //await Future.delayed(Duration(seconds: 10));
-  LocationUpdater.start();
+  //LocationUpdater.start();
 
   Future<Map<String,dynamic>> config = UrlFunctions.getConfig(env);
   config.then((jsonMap) {
